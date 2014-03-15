eaae8dfc0b8e8a46f01e2ebfd04bafd275ddcce3
Frédéric Guillot <fguillot@users.noreply.github.com>
2014-03-14 21:16:37 -0400

    Update to the last version of password_compat

--- vendor/password.php
+++ vendor/password.php
@@ -7,221 +7,273 @@
  * @copyright 2012 The Authors
  */
 
-if (!defined('PASSWORD_BCRYPT')) {
+namespace {
 
-	define('PASSWORD_BCRYPT', 1);
-	define('PASSWORD_DEFAULT', PASSWORD_BCRYPT);
+if (!defined('PASSWORD_DEFAULT')) {
 
-	if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    define('PASSWORD_BCRYPT', 1);
+    define('PASSWORD_DEFAULT', PASSWORD_BCRYPT);
 
-		define('PASSWORD_PREFIX', '$2a$');
-	}
-	else {
+    /**
+     * Hash the password using the specified algorithm
+     *
+     * @param string $password The password to hash
+     * @param int    $algo     The algorithm to use (Defined by PASSWORD_* constants)
+     * @param array  $options  The options for the algorithm to use
+     *
+     * @return string|false The hashed password, or false on error.
+     */
+    function password_hash($password, $algo, array $options = array()) {
+        if (!function_exists('crypt')) {
+            trigger_error("Crypt must be loaded for password_hash to function", E_USER_WARNING);
+            return null;
+        }
+        if (!is_string($password)) {
+            trigger_error("password_hash(): Password must be a string", E_USER_WARNING);
+            return null;
+        }
+        if (!is_int($algo)) {
+            trigger_error("password_hash() expects parameter 2 to be long, " . gettype($algo) . " given", E_USER_WARNING);
+            return null;
+        }
+        $resultLength = 0;
+        switch ($algo) {
+            case PASSWORD_BCRYPT:
+                // Note that this is a C constant, but not exposed to PHP, so we don't define it here.
+                $cost = 10;
+                if (isset($options['cost'])) {
+                    $cost = $options['cost'];
+                    if ($cost < 4 || $cost > 31) {
+                        trigger_error(sprintf("password_hash(): Invalid bcrypt cost parameter specified: %d", $cost), E_USER_WARNING);
+                        return null;
+                    }
+                }
+                // The length of salt to generate
+                $raw_salt_len = 16;
+                // The length required in the final serialization
+                $required_salt_len = 22;
+                $hash_format = sprintf("$2y$%02d$", $cost);
+                // The expected length of the final crypt() output
+                $resultLength = 60;
+                break;
+            default:
+                trigger_error(sprintf("password_hash(): Unknown password hashing algorithm: %s", $algo), E_USER_WARNING);
+                return null;
+        }
+        $salt_requires_encoding = false;
+        if (isset($options['salt'])) {
+            switch (gettype($options['salt'])) {
+                case 'NULL':
+                case 'boolean':
+                case 'integer':
+                case 'double':
+                case 'string':
+                    $salt = (string) $options['salt'];
+                    break;
+                case 'object':
+                    if (method_exists($options['salt'], '__tostring')) {
+                        $salt = (string) $options['salt'];
+                        break;
+                    }
+                case 'array':
+                case 'resource':
+                default:
+                    trigger_error('password_hash(): Non-string salt parameter supplied', E_USER_WARNING);
+                    return null;
+            }
+            if (PasswordCompat\binary\_strlen($salt) < $required_salt_len) {
+                trigger_error(sprintf("password_hash(): Provided salt is too short: %d expecting %d", PasswordCompat\binary\_strlen($salt), $required_salt_len), E_USER_WARNING);
+                return null;
+            } elseif (0 == preg_match('#^[a-zA-Z0-9./]+$#D', $salt)) {
+                $salt_requires_encoding = true;
+            }
+        } else {
+            $buffer = '';
+            $buffer_valid = false;
+            if (function_exists('mcrypt_create_iv') && !defined('PHALANGER')) {
+                $buffer = mcrypt_create_iv($raw_salt_len, MCRYPT_DEV_URANDOM);
+                if ($buffer) {
+                    $buffer_valid = true;
+                }
+            }
+            if (!$buffer_valid && function_exists('openssl_random_pseudo_bytes')) {
+                $buffer = openssl_random_pseudo_bytes($raw_salt_len);
+                if ($buffer) {
+                    $buffer_valid = true;
+                }
+            }
+            if (!$buffer_valid && @is_readable('/dev/urandom')) {
+                $f = fopen('/dev/urandom', 'r');
+                $read = PasswordCompat\binary\_strlen($buffer);
+                while ($read < $raw_salt_len) {
+                    $buffer .= fread($f, $raw_salt_len - $read);
+                    $read = PasswordCompat\binary\_strlen($buffer);
+                }
+                fclose($f);
+                if ($read >= $raw_salt_len) {
+                    $buffer_valid = true;
+                }
+            }
+            if (!$buffer_valid || PasswordCompat\binary\_strlen($buffer) < $raw_salt_len) {
+                $bl = PasswordCompat\binary\_strlen($buffer);
+                for ($i = 0; $i < $raw_salt_len; $i++) {
+                    if ($i < $bl) {
+                        $buffer[$i] = $buffer[$i] ^ chr(mt_rand(0, 255));
+                    } else {
+                        $buffer .= chr(mt_rand(0, 255));
+                    }
+                }
+            }
+            $salt = $buffer;
+            $salt_requires_encoding = true;
+        }
+        if ($salt_requires_encoding) {
+            // encode string with the Base64 variant used by crypt
+            $base64_digits =
+                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+            $bcrypt64_digits =
+                './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-		define('PASSWORD_PREFIX', '$2y$');
-	}
+            $base64_string = base64_encode($salt);
+            $salt = strtr(rtrim($base64_string, '='), $base64_digits, $bcrypt64_digits);
+        }
+        $salt = PasswordCompat\binary\_substr($salt, 0, $required_salt_len);
 
-	/**
-	 * Hash the password using the specified algorithm
-	 *
-	 * @param string $password The password to hash
-	 * @param int    $algo     The algorithm to use (Defined by PASSWORD_* constants)
-	 * @param array  $options  The options for the algorithm to use
-	 *
-	 * @return string|false The hashed password, or false on error.
-	 */
-	function password_hash($password, $algo, array $options = array()) {
-		if (!function_exists('crypt')) {
-			trigger_error("Crypt must be loaded for password_hash to function", E_USER_WARNING);
-			return null;
-		}
-		if (!is_string($password)) {
-			trigger_error("password_hash(): Password must be a string", E_USER_WARNING);
-			return null;
-		}
-		if (!is_int($algo)) {
-			trigger_error("password_hash() expects parameter 2 to be long, " . gettype($algo) . " given", E_USER_WARNING);
-			return null;
-		}
-		switch ($algo) {
-			case PASSWORD_BCRYPT:
-				// Note that this is a C constant, but not exposed to PHP, so we don't define it here.
-				$cost = 10;
-				if (isset($options['cost'])) {
-					$cost = $options['cost'];
-					if ($cost < 4 || $cost > 31) {
-						trigger_error(sprintf("password_hash(): Invalid bcrypt cost parameter specified: %d", $cost), E_USER_WARNING);
-						return null;
-					}
-				}
-				$required_salt_len = 22;
-				$hash_format = sprintf("%s%02d$", PASSWORD_PREFIX, $cost);
-				break;
-			default:
-				trigger_error(sprintf("password_hash(): Unknown password hashing algorithm: %s", $algo), E_USER_WARNING);
-				return null;
-		}
-		if (isset($options['salt'])) {
-			switch (gettype($options['salt'])) {
-				case 'NULL':
-				case 'boolean':
-				case 'integer':
-				case 'double':
-				case 'string':
-					$salt = (string) $options['salt'];
-					break;
-				case 'object':
-					if (method_exists($options['salt'], '__tostring')) {
-						$salt = (string) $options['salt'];
-						break;
-					}
-				case 'array':
-				case 'resource':
-				default:
-					trigger_error('password_hash(): Non-string salt parameter supplied', E_USER_WARNING);
-					return null;
-			}
-			if (strlen($salt) < $required_salt_len) {
-				trigger_error(sprintf("password_hash(): Provided salt is too short: %d expecting %d", strlen($salt), $required_salt_len), E_USER_WARNING);
-				return null;
-			} elseif (0 == preg_match('#^[a-zA-Z0-9./]+$#D', $salt)) {
-				$salt = str_replace('+', '.', base64_encode($salt));
-			}
-		} else {
-			$buffer = '';
-			$raw_length = (int) ($required_salt_len * 3 / 4 + 1);
-			$buffer_valid = false;
-			if (function_exists('mcrypt_create_iv') && !defined('PHALANGER')) {
-				$buffer = mcrypt_create_iv($raw_length, MCRYPT_DEV_URANDOM);
-				if ($buffer) {
-					$buffer_valid = true;
-				}
-			}
-			if (!$buffer_valid && function_exists('openssl_random_pseudo_bytes')) {
-				$buffer = openssl_random_pseudo_bytes($raw_length);
-				if ($buffer) {
-					$buffer_valid = true;
-				}
-			}
-			if (!$buffer_valid && is_readable('/dev/urandom')) {
-				$f = fopen('/dev/urandom', 'r');
-				$read = strlen($buffer);
-				while ($read < $raw_length) {
-					$buffer .= fread($f, $raw_length - $read);
-					$read = strlen($buffer);
-				}
-				fclose($f);
-				if ($read >= $raw_length) {
-					$buffer_valid = true;
-				}
-			}
-			if (!$buffer_valid || strlen($buffer) < $raw_length) {
-				$bl = strlen($buffer);
-				for ($i = 0; $i < $raw_length; $i++) {
-					if ($i < $bl) {
-						$buffer[$i] = $buffer[$i] ^ chr(mt_rand(0, 255));
-					} else {
-						$buffer .= chr(mt_rand(0, 255));
-					}
-				}
-			}
-			$salt = str_replace('+', '.', base64_encode($buffer));
+        $hash = $hash_format . $salt;
 
-		}
-		$salt = substr($salt, 0, $required_salt_len);
+        $ret = crypt($password, $hash);
 
-		$hash = $hash_format . $salt;
+        if (!is_string($ret) || PasswordCompat\binary\_strlen($ret) != $resultLength) {
+            return false;
+        }
 
-		$ret = crypt($password, $hash);
+        return $ret;
+    }
 
-		if (!is_string($ret) || strlen($ret) <= 13) {
-			return false;
-		}
+    /**
+     * Get information about the password hash. Returns an array of the information
+     * that was used to generate the password hash.
+     *
+     * array(
+     *    'algo' => 1,
+     *    'algoName' => 'bcrypt',
+     *    'options' => array(
+     *        'cost' => 10,
+     *    ),
+     * )
+     *
+     * @param string $hash The password hash to extract info from
+     *
+     * @return array The array of information about the hash.
+     */
+    function password_get_info($hash) {
+        $return = array(
+            'algo' => 0,
+            'algoName' => 'unknown',
+            'options' => array(),
+        );
+        if (PasswordCompat\binary\_substr($hash, 0, 4) == '$2y$' && PasswordCompat\binary\_strlen($hash) == 60) {
+            $return['algo'] = PASSWORD_BCRYPT;
+            $return['algoName'] = 'bcrypt';
+            list($cost) = sscanf($hash, "$2y$%d$");
+            $return['options']['cost'] = $cost;
+        }
+        return $return;
+    }
 
-		return $ret;
-	}
+    /**
+     * Determine if the password hash needs to be rehashed according to the options provided
+     *
+     * If the answer is true, after validating the password using password_verify, rehash it.
+     *
+     * @param string $hash    The hash to test
+     * @param int    $algo    The algorithm used for new password hashes
+     * @param array  $options The options array passed to password_hash
+     *
+     * @return boolean True if the password needs to be rehashed.
+     */
+    function password_needs_rehash($hash, $algo, array $options = array()) {
+        $info = password_get_info($hash);
+        if ($info['algo'] != $algo) {
+            return true;
+        }
+        switch ($algo) {
+            case PASSWORD_BCRYPT:
+                $cost = isset($options['cost']) ? $options['cost'] : 10;
+                if ($cost != $info['options']['cost']) {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
 
-	/**
-	 * Get information about the password hash. Returns an array of the information
-	 * that was used to generate the password hash.
-	 *
-	 * array(
-	 *    'algo' => 1,
-	 *    'algoName' => 'bcrypt',
-	 *    'options' => array(
-	 *        'cost' => 10,
-	 *    ),
-	 * )
-	 *
-	 * @param string $hash The password hash to extract info from
-	 *
-	 * @return array The array of information about the hash.
-	 */
-	function password_get_info($hash) {
-		$return = array(
-			'algo' => 0,
-			'algoName' => 'unknown',
-			'options' => array(),
-		);
-		if (substr($hash, 0, 4) == PASSWORD_PREFIX && strlen($hash) == 60) {
-			$return['algo'] = PASSWORD_BCRYPT;
-			$return['algoName'] = 'bcrypt';
-			list($cost) = sscanf($hash, PASSWORD_PREFIX."%d$");
-			$return['options']['cost'] = $cost;
-		}
-		return $return;
-	}
-
-	/**
-	 * Determine if the password hash needs to be rehashed according to the options provided
-	 *
-	 * If the answer is true, after validating the password using password_verify, rehash it.
-	 *
-	 * @param string $hash    The hash to test
-	 * @param int    $algo    The algorithm used for new password hashes
-	 * @param array  $options The options array passed to password_hash
-	 *
-	 * @return boolean True if the password needs to be rehashed.
-	 */
-	function password_needs_rehash($hash, $algo, array $options = array()) {
-		$info = password_get_info($hash);
-		if ($info['algo'] != $algo) {
-			return true;
-		}
-		switch ($algo) {
-			case PASSWORD_BCRYPT:
-				$cost = isset($options['cost']) ? $options['cost'] : 10;
-				if ($cost != $info['options']['cost']) {
-					return true;
-				}
-				break;
-		}
-		return false;
-	}
-
-	/**
-	 * Verify a password against a hash using a timing attack resistant approach
-	 *
-	 * @param string $password The password to verify
-	 * @param string $hash     The hash to verify against
-	 *
-	 * @return boolean If the password matches the hash
-	 */
+    /**
+     * Verify a password against a hash using a timing attack resistant approach
+     *
+     * @param string $password The password to verify
+     * @param string $hash     The hash to verify against
+     *
+     * @return boolean If the password matches the hash
+     */
     function password_verify($password, $hash) {
-		if (!function_exists('crypt')) {
-			trigger_error("Crypt must be loaded for password_verify to function", E_USER_WARNING);
-			return false;
-		}
-		$ret = crypt($password, $hash);
-		if (!is_string($ret) || strlen($ret) != strlen($hash) || strlen($ret) <= 13) {
-			return false;
-		}
+        if (!function_exists('crypt')) {
+            trigger_error("Crypt must be loaded for password_verify to function", E_USER_WARNING);
+            return false;
+        }
+        $ret = crypt($password, $hash);
+        if (!is_string($ret) || PasswordCompat\binary\_strlen($ret) != PasswordCompat\binary\_strlen($hash) || PasswordCompat\binary\_strlen($ret) <= 13) {
+            return false;
+        }
 
-		$status = 0;
-		for ($i = 0; $i < strlen($ret); $i++) {
-			$status |= (ord($ret[$i]) ^ ord($hash[$i]));
-		}
+        $status = 0;
+        for ($i = 0; $i < PasswordCompat\binary\_strlen($ret); $i++) {
+            $status |= (ord($ret[$i]) ^ ord($hash[$i]));
+        }
 
-		return $status === 0;
-	}
+        return $status === 0;
+    }
+}
+
+}
+
+namespace PasswordCompat\binary {
+    /**
+     * Count the number of bytes in a string
+     *
+     * We cannot simply use strlen() for this, because it might be overwritten by the mbstring extension.
+     * In this case, strlen() will count the number of *characters* based on the internal encoding. A
+     * sequence of bytes might be regarded as a single multibyte character.
+     *
+     * @param string $binary_string The input string
+     *
+     * @internal
+     * @return int The number of bytes
+     */
+    function _strlen($binary_string) {
+           if (function_exists('mb_strlen')) {
+               return mb_strlen($binary_string, '8bit');
+           }
+           return strlen($binary_string);
+    }
+
+    /**
+     * Get a substring based on byte limits
+     *
+     * @see _strlen()
+     *
+     * @param string $binary_string The input string
+     * @param int    $start
+     * @param int    $length
+     *
+     * @internal
+     * @return string The substring
+     */
+    function _substr($binary_string, $start, $length) {
+       if (function_exists('mb_substr')) {
+           return mb_substr($binary_string, $start, $length, '8bit');
+       }
+       return substr($binary_string, $start, $length);
+   }
+
 }
