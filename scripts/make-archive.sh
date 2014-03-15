395a8a5f2a4976b80432a0d71d51d1bad3d01a83
Frédéric Guillot <fguillot@users.noreply.github.com>
2014-03-15 18:55:35 -0400

    Improve make-archive.sh script

--- scripts/make-archive.sh
+++ scripts/make-archive.sh
@@ -7,7 +7,7 @@ cd /tmp
 rm -rf /tmp/$APP /tmp/$APP-*.zip 2>/dev/null
 git clone git@github.com:fguillot/$APP.git
 rm -rf $APP/data/*.sqlite $APP/.git $APP/.gitignore $APP/scripts $APP/tests $APP/Vagrantfile
-sed -i.bak s/master/$VERSION/g $APP/models/base.php && rm -f $APP/models/*.bak
+sed -i.bak s/master/$VERSION/g $APP/common.php && rm -f $APP/*.bak
 zip -r $APP-$VERSION.zip $APP
 mv $APP-*.zip ~/Devel/websites/$APP
 rm -rf /tmp/$APP 2>/dev/null
