21cf7dd619ecfb2ed424d9508b8fc24247c6e701
Frédéric Guillot <fguillot@users.noreply.github.com>
2014-04-20 19:58:57 -0400

    Add default config file

---
+++ config.default.php
@@ -0,0 +1,33 @@
+<?php
+
+// Auto-refresh frequency in seconds for the public board view (60 seconds by default)
+define('AUTO_REFRESH_DURATION', 60);
+
+// Database driver: sqlite or mysql (sqlite by default)
+define('DB_DRIVER', 'sqlite');
+
+// Mysql username
+define('DB_USERNAME', 'root');
+
+// Mysql password
+define('DB_PASSWORD', '');
+
+// Mysql hostname
+define('DB_HOSTNAME', 'localhost');
+
+// Mysql database name
+define('DB_NAME', 'kanboard');
+
+// Enable LDAP authentication (false by default)
+define('LDAP_AUTH', false);
+
+// LDAP server hostname
+define('LDAP_SERVER', '');
+
+// LDAP server port (389 by default)
+define('LDAP_PORT', 389);
+
+// User LDAP DN
+// Example for ActiveDirectory: 'MYDOMAIN\\%s' or '%s@mydomain.local'
+// Example for OpenLDAP: 'uid=%s,ou=People,dc=example,dc=com'
+define('LDAP_USER_DN', '%s');
