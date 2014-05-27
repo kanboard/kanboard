ea1f1f2740e9b3381d88e9287932c04a10e8126f
Frédéric Guillot <fred@kanboard.net>
2014-05-27 18:53:17 -0400

    Remove useless files when building the archive

--- scripts/make-archive.sh
+++ scripts/make-archive.sh
@@ -6,7 +6,7 @@ APP="kanboard"
 cd /tmp
 rm -rf /tmp/$APP /tmp/$APP-*.zip 2>/dev/null
 git clone git@github.com:fguillot/$APP.git
-rm -rf $APP/data/*.sqlite $APP/.git $APP/.gitignore $APP/scripts $APP/tests $APP/Vagrantfile
+rm -rf $APP/data/*.sqlite $APP/.git $APP/.gitignore $APP/scripts $APP/tests $APP/Vagrantfile $APP/.*.yml $APP/phpunit.xml $APP/README.markdown $APP/docs
 sed -i.bak s/master/$VERSION/g $APP/app/common.php && rm -f $APP/app/*.bak
 zip -r $APP-$VERSION.zip $APP
 mv $APP-*.zip ~/Devel/websites/$APP
