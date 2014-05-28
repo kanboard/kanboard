b6b124aadde36b545b166049fde2cfbbc8b0a553
Frédéric Guillot <fred@kanboard.net>
2014-05-28 18:36:09 -0400

    Improve session destroy

--- app/Core/Session.php
+++ app/Core/Session.php
@@ -66,6 +66,25 @@ class Session
      */
     public function close()
     {
+        // Flush all sessions variables
+        $_SESSION = array();
+
+        // Destroy the session cookie
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        // Destroy session data
         session_destroy();
     }
 
