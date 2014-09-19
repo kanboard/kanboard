0a9a4ba0393d40f3bcba4e4e531bd0d51d2c3aff
Frédéric Guillot <fred@kanboard.net>
2014-09-19 15:29:39 +0200

    Improve make-archive.sh and update swiftmailer

--- scripts/make-archive.sh
+++ scripts/make-archive.sh
@@ -5,9 +5,24 @@ APP="kanboard"
 
 cd /tmp
 rm -rf /tmp/$APP /tmp/$APP-*.zip 2>/dev/null
-git clone https://github.com/fguillot/$APP.git
-rm -rf $APP/data/*.sqlite $APP/.git $APP/.gitignore $APP/scripts $APP/tests $APP/Vagrantfile $APP/.*.yml $APP/phpunit.xml $APP/README.markdown $APP/docs
+git clone --depth 1 https://github.com/fguillot/$APP.git
+
+rm -rf $APP/data/*.sqlite \
+      $APP/.git $APP/.gitignore \
+      $APP/scripts \
+      $APP/tests \
+      $APP/Vagrantfile \
+      $APP/.*.yml \
+      $APP/README.markdown \
+      $APP/docs
+
 sed -i.bak s/master/$VERSION/g $APP/app/common.php && rm -f $APP/app/*.bak
 zip -r $APP-$VERSION.zip $APP
-mv $APP-*.zip ~/Devel/websites/$APP
+
+mv $APP-$VERSION.zip ~/Devel/websites/$APP
+
+cd ~/Devel/websites/$APP/
+unlink $APP-latest.zip
+ln -s $APP-$VERSION.zip $APP-latest.zip
+
 rm -rf /tmp/$APP 2>/dev/null
