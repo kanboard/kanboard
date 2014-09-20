8022b38d5c7265d0c1731e29bb473b666e0f5110
Dunstkreis <Andreas.Riddering@t-online.de>
2014-09-20 12:06:46 +0200

    added bool HEADER_STS
    Added switch for enable/disable adding the STS header

--- config.default.php
+++ config.default.php
@@ -109,3 +109,6 @@ define('REVERSE_PROXY_DEFAULT_ADMIN', '');
 
 // Default domain to use for setting the email address
 define('REVERSE_PROXY_DEFAULT_DOMAIN', '');
+
+// Enable/disable adding STS header
+define('HEADER_STS', false);
