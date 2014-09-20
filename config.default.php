77db09fab0e88d48b89a3869385151ef933b6c72
Dunstkreis <Andreas.Riddering@t-online.de>
2014-09-20 12:07:38 +0200

    set default to true
    to keep existing behavior

--- config.default.php
+++ config.default.php
@@ -111,4 +111,4 @@ define('REVERSE_PROXY_DEFAULT_ADMIN', '');
 define('REVERSE_PROXY_DEFAULT_DOMAIN', '');
 
 // Enable/disable adding STS header
-define('HEADER_STS', false);
+define('HEADER_STS', true);
