eab8073a6937ac3b378147d039c1a7a6c97d5f2d
Oliver Bertuch <o.bertuch@fz-juelich.de>
2014-10-23 12:05:20 +0200

    Adding edit links for board and project to board_index and putting some nice FA icons before board links on board_index.

--- app/Templates/board_index.php
+++ app/Templates/board_index.php
@@ -2,6 +2,9 @@
 
     <div class="page-header board">
         <h2>
+            <?php if(Helper\is_admin()): ?>
+            <a href="?controller=project&amp;action=show&amp;project_id=<?= $current_project_id ?>"><i class="fa fa-cog"></i></a>
+            <?php endif ?>
             <?= t('Project "%s"', $current_project_name) ?>
         </h2>
     </div>
@@ -17,9 +20,12 @@
                 <?= Helper\form_select('category_id', $categories, $filters) ?>
             </li>
             <li><a href="#" id="filter-due-date"><?= t('Filter by due date') ?></a></li>
-            <li><a href="?controller=project&amp;action=search&amp;project_id=<?= $current_project_id ?>"><?= t('Search') ?></a></li>
-            <li><a href="?controller=project&amp;action=tasks&amp;project_id=<?= $current_project_id ?>"><?= t('Completed tasks') ?></a></li>
-            <li><a href="?controller=project&amp;action=activity&amp;project_id=<?= $current_project_id ?>"><?= t('Activity') ?></a></li>
+            <li><i class="fa fa-search"></i> <a href="?controller=project&amp;action=search&amp;project_id=<?= $current_project_id ?>"><?= t('Search') ?></a></li>
+            <li><i class="fa fa-check-square-o"></i> <a href="?controller=project&amp;action=tasks&amp;project_id=<?= $current_project_id ?>"><?= t('Completed tasks') ?></a></li>
+            <li><i class="fa fa-dashboard"></i> <a href="?controller=project&amp;action=activity&amp;project_id=<?= $current_project_id ?>"><?= t('Activity') ?></a></li>
+            <?php if(Helper\is_admin()): ?>
+            <li><i class="fa fa-cog"></i> <a href="?controller=board&amp;action=edit&amp;project_id=<?= $current_project_id ?>"><?= t('Edit board') ?></a></li>
+            <?php endif ?>
         </ul>
     </div>
 
