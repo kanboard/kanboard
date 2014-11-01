db8c8c34dc9dc4c2348ec992241a5d5203e2d4eb
Frédéric Guillot <fred@kanboard.net>
2014-11-01 16:44:55 -0400

    Fix PHP notice for a template

--- app/Template/notification_footer.php
+++ app/Template/notification_footer.php
@@ -1,6 +1,6 @@
 <hr/>
 Kanboard
 
-<?php if ($application_url): ?>
+<?php if (isset($application_url) && ! empty($application_url)): ?>
     - <a href="<?= $application_url.'?controller=task&action=show&task_id='.$task['id'] ?>"><?= t('view the task on Kanboard') ?></a>.
 <?php endif ?>
