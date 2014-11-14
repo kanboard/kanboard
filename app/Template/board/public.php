84770123b4784a74b804a00facaea5bf9ae14c9f
Frédéric Guillot <fred@kanboard.net>
2014-11-13 19:46:45 -0500

    Rename template file, fix #394

--- app/Template/board/public.php
+++ app/Template/board/public.php
@@ -21,7 +21,12 @@
                     <?php foreach ($column['tasks'] as $task): ?>
                     <div class="task-board task-<?= $task['color_id'] ?>">
 
-                        <?= Helper\template('board/task', array('task' => $task, 'categories' => $categories, 'not_editable' => true, 'project' => $project)) ?>
+                        <?= Helper\template('board/task', array(
+                            'task' => $task,
+                            'categories' => $categories,
+                            'not_editable' => true,
+                            'project' => $project
+                        )) ?>
 
                     </div>
                     <?php endforeach ?>
