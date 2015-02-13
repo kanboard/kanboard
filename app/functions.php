0c60489b08379c7fa5e7fb2417e0ad20b4a1dadf
ashbike <helloashbike@gmail.com>
2015-02-13 11:45:55 +0530

    Task age and days in column shows hours if < 1 day

--- app/functions.php
+++ app/functions.php
@@ -66,3 +66,28 @@ function dt($format, $timestamp)
 function p($value, $t1, $t2) {
     return $value > 1 ? $t2 : $t1;
 }
+
+/**
+ * Get the age of an item in quasi human readable format.
+ * It's in this format: <1h , NNh, NNd
+ *
+ * @access public
+ * @param int $time
+ *        	Unix timestamp of the artifact for which age will be calculated
+ * @param int $currenttime
+ *        	Comepare with timestamp. Default current unix timestamp
+ * @return string
+ */
+function getAgeShort($time, $currenttime = NULL) {
+	if (! $currenttime) {
+		$currenttime = time ();
+	}
+	$diff = $currenttime - $time;
+
+	if ($diff < 3600)
+		return "<1h";
+	elseif ($diff < 86400) {
+		return intval ( ($diff / 3600) ) . "h";
+	}
+	return intval ( ((floor ( $currenttime ) - floor ( $time )) / 86400) ) . "d";
+}
