900e3fa1fd0a41e1617cd42a83b34d4c39d1730f
Frederic Guillot <fred@kanboard.net>
2015-03-09 21:48:45 -0400

    Bust subtask title max length to 255 characters

--- app/Model/Subtask.php
+++ app/Model/Subtask.php
@@ -456,7 +456,7 @@ class Subtask extends Base
         return array(
             new Validators\Integer('id', t('The subtask id must be an integer')),
             new Validators\Integer('task_id', t('The task id must be an integer')),
-            new Validators\MaxLength('title', t('The maximum length is %d characters', 100), 100),
+            new Validators\MaxLength('title', t('The maximum length is %d characters', 255), 255),
             new Validators\Integer('user_id', t('The user id must be an integer')),
             new Validators\Integer('status', t('The status must be an integer')),
             new Validators\Numeric('time_estimated', t('The time must be a numeric value')),
