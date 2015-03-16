a4c51ddf640429956beafc1df71e2a79ccc2f7f9
Olivier Maridat <olivier.maridat@trialog.com>
2015-03-16 14:50:27 +0100

    Fix TaskLink unit test

--- tests/units/TaskLinkTest.php
+++ tests/units/TaskLinkTest.php
@@ -92,17 +92,47 @@ class TaskLinkTest extends Base
     public function testValidation()
     {
         $tl = new TaskLink($this->container);
+        
+        // Create tasks
+        $tl = new TaskLink($this->container);
+        $p = new Project($this->container);
+        $tc = new TaskCreation($this->container);
+        
+        $this->assertEquals(1, $p->create(array('name' => 'test')));
+        $this->assertEquals(1, $tc->create(array('project_id' => 1, 'title' => 'A')));
+        $this->assertEquals(2, $tc->create(array('project_id' => 1, 'title' => 'B')));
+        $this->assertTrue($tl->create(1, 2, 2));
+        
+        $links = $tl->getLinks(1);
+        $this->assertNotEmpty($links);
+        $this->assertCount(1, $links);
+        $this->assertEquals('blocks', $links[0]['label']);
+        $this->assertEquals('B', $links[0]['title']);
+        $this->assertEquals(2, $links[0]['task_id']);
+        $this->assertEquals(1, $links[0]['is_active']);
+        
+        $links = $tl->getLinks(2);
+        $this->assertNotEmpty($links);
+        $this->assertCount(1, $links);
+        $this->assertEquals('is blocked by', $links[0]['label']);
+        $this->assertEquals('A', $links[0]['title']);
+        $this->assertEquals(1, $links[0]['task_id']);
+        $this->assertEquals(1, $links[0]['is_active']);
 
-        $r = $tl->validateCreation(array('task_id' => 1, 'link_id' => 1, 'title' => 'a'));
+        // Check validation
+        $r = $tl->validateCreation(array('task_id' => 1, 'link_id' => 1, 'opposite_task_id' => 2));
         $this->assertTrue($r[0]);
 
         $r = $tl->validateCreation(array('task_id' => 1, 'link_id' => 1));
         $this->assertFalse($r[0]);
 
-        $r = $tl->validateCreation(array('task_id' => 1, 'title' => 'a'));
+        $r = $tl->validateCreation(array('task_id' => 1, 'opposite_task_id' => 2));
         $this->assertFalse($r[0]);
 
-        $r = $tl->validateCreation(array('link_id' => 1, 'title' => 'a'));
+        $r = $tl->validateCreation(array('task_id' => 1, 'opposite_task_id' => 2));
+        $this->assertFalse($r[0]);
+
+        $r = $tl->validateCreation(array('task_id' => 1, 'link_id' => 1, 'opposite_task_id' => 1));
         $this->assertFalse($r[0]);
     }
 }
