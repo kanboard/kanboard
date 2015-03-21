bcb39ad4db0f4c1d9bf35437852ee0b74e1680ce
Frederic Guillot <fred@kanboard.net>
2015-03-21 18:25:35 -0400

    Add option to enable/disable iframe inclusion http header

--- config.default.php
+++ config.default.php
@@ -124,3 +124,6 @@ define('REVERSE_PROXY_DEFAULT_DOMAIN', '');
 
 // Enable or disable "Strict-Transport-Security" HTTP header
 define('ENABLE_HSTS', true);
+
+// Enable or disable "X-Frame-Options: DENY" HTTP header
+define('ENABLE_XFRAME', true);
