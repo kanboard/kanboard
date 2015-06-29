6169dd7fe796bdbbda6ad2095084f0f912ffcca0
Wiebke Meyer <wiebke.meyer@trimm.nl>
2015-06-29 14:37:54 +0200

    added the feature to open 'create another task' inside a popup

--- app/Controller/Task.php
+++ app/Controller/Task.php
@@ -163,8 +163,7 @@ class Task extends Base
                 if (isset($values['another_task']) && $values['another_task'] == 1) {
                     unset($values['title']);
                     unset($values['description']);
-                    $this->response->redirect('?controller=task&action=create&'.http_build_query($values));
-                }
+                    $this->response->redirect('?controller=board&action=show&project_id='.$project['id'].'&redirect=true&category_id='.$values['category_id'].'&column_id='.$values['column_id'].'&color_id='.$values['color_id'].'&creator_id='.$values['creator_id']);                }
                 else {
                     $this->response->redirect('?controller=board&action=show&project_id='.$project['id']);
                 }
@@ -173,8 +172,9 @@ class Task extends Base
                 $this->session->flashError(t('Unable to create your task.'));
             }
         }
-
-        $this->create($values, $errors);
+        else {
+            $this->create($values, $errors);
+        }
     }
 
     /**
--- app/Template/board/show.php
+++ app/Template/board/show.php
@@ -1,7 +1,7 @@
 <div id="board-container">
     <?php if (isset($not_editable)): ?>
-        <table id="board" class="board-project-<?= $project['id'] ?>">
-    <?php else: ?>
+    <table id="board" class="board-project-<?= $project['id'] ?>">
+        <?php else: ?>
         <table id="board"
                class="board-project-<?= $project['id'] ?>"
                data-project-id="<?= $project['id'] ?>"
@@ -9,24 +9,25 @@
                data-save-url="<?= $this->url->href('board', 'save', array('project_id' => $project['id'])) ?>"
                data-check-url="<?= $this->url->href('board', 'check', array('project_id' => $project['id'], 'timestamp' => time())) ?>"
                data-task-creation-url="<?= $this->url->href('task', 'create', array('project_id' => $project['id'])) ?>"
-        >
-    <?php endif ?>
+            >
+            <?php endif ?>
 
-    <?php foreach ($swimlanes as $swimlane): ?>
-        <?php if (empty($swimlane['columns'])): ?>
-            <p class="alert alert-error"><?= t('There is no column in your project!') ?></p>
-            <?php break ?>
-        <?php else: ?>
-            <?= $this->render('board/swimlane', array(
-                'project' => $project,
-                'swimlane' => $swimlane,
-                'board_highlight_period' => $board_highlight_period,
-                'categories_listing' => $categories_listing,
-                'categories_description' => $categories_description,
-                'hide_swimlane' => count($swimlanes) === 1,
-                'not_editable' => isset($not_editable),
-            )) ?>
-        <?php endif ?>
-    <?php endforeach ?>
-    </table>
+            <?php foreach ($swimlanes as $swimlane): ?>
+                <?php if (empty($swimlane['columns'])): ?>
+                    <p class="alert alert-error"><?= t('There is no column in your project!') ?></p>
+                    <?php break ?>
+                <?php else: ?>
+                    <?= $this->render('board/swimlane', array(
+                        'project' => $project,
+                        'swimlane' => $swimlane,
+                        'board_highlight_period' => $board_highlight_period,
+                        'categories_listing' => $categories_listing,
+                        'categories_description' => $categories_description,
+                        'hide_swimlane' => count($swimlanes) === 1,
+                        'not_editable' => isset($not_editable),
+                    )) ?>
+                <?php endif ?>
+            <?php endforeach ?>
+        </table>
+        <script src="../../../assets/js/popupKanboard.js"></script>
 </div>
