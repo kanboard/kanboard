345c4caea49d50063d0af65440946002fe79912e
Frederic Guillot <fred@kanboard.net>
2015-08-30 09:44:15 -0400

    Add missing active class for sidebars

--- app/Template/budget/sidebar.php
+++ app/Template/budget/sidebar.php
@@ -1,13 +1,13 @@
 <div class="sidebar">
     <h2><?= t('Budget') ?></h2>
     <ul>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'index' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Budget overview'), 'budget', 'index', array('project_id' => $project['id'])) ?>
         </li>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'create' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Budget lines'), 'budget', 'create', array('project_id' => $project['id'])) ?>
         </li>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'breakdown' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Cost breakdown'), 'budget', 'breakdown', array('project_id' => $project['id'])) ?>
         </li>
     </ul>
--- app/Template/export/sidebar.php
+++ app/Template/export/sidebar.php
@@ -1,16 +1,16 @@
 <div class="sidebar">
     <h2><?= t('Exports') ?></h2>
     <ul>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'tasks' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Tasks'), 'export', 'tasks', array('project_id' => $project['id'])) ?>
         </li>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'subtasks' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Subtasks'), 'export', 'subtasks', array('project_id' => $project['id'])) ?>
         </li>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'transitions' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Task transitions'), 'export', 'transitions', array('project_id' => $project['id'])) ?>
         </li>
-        <li>
+        <li <?= $this->app->getRouterAction() === 'summary' ? 'class="active"' : '' ?>>
             <?= $this->url->link(t('Daily project summary'), 'export', 'summary', array('project_id' => $project['id'])) ?>
         </li>
     </ul>
