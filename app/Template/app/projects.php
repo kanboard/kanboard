8b086e87405c779790c499c793d66071f32b661c
Frederic Guillot <fred@kanboard.net>
2015-10-07 19:30:16 -0400

    Include more shortcut links into the view "My projects"

--- app/Template/app/projects.php
+++ app/Template/app/projects.php
@@ -6,8 +6,9 @@
 <?php else: ?>
     <table class="table-fixed table-small">
         <tr>
-            <th class="column-8"><?= $paginator->order('Id', 'id') ?></th>
-            <th class="column-20"><?= $paginator->order(t('Project'), 'name') ?></th>
+            <th class="column-5"><?= $paginator->order('Id', 'id') ?></th>
+            <th class="column-3"><?= $paginator->order('<i class="fa fa-lock fa-fw" title="'.t('Private project').'"></i>', 'is_private') ?></th>
+            <th class="column-25"><?= $paginator->order(t('Project'), 'name') ?></th>
             <th><?= t('Columns') ?></th>
         </tr>
         <?php foreach ($paginator->getCollection() as $project): ?>
@@ -16,10 +17,17 @@
                 <?= $this->url->link('#'.$project['id'], 'board', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link') ?>
             </td>
             <td>
+                <?php if ($project['is_private']): ?>
+                    <i class="fa fa-lock fa-fw" title="<?= t('Private project') ?>"></i>
+                <?php endif ?>
+            </td>
+            <td>
                 <?php if ($this->user->isProjectManagementAllowed($project['id'])): ?>
-                    <?= $this->url->link('<i class="fa fa-cog"></i>', 'project', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Settings')) ?>&nbsp;
+                    <?= $this->url->link('<i class="fa fa-cog"></i>', 'project', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Settings')) ?>
+                    <?= $this->url->link('<i class="fa fa-sliders fa-fw"></i>', 'gantt', 'project', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Gantt chart')) ?>
                 <?php endif ?>
 
+                <?= $this->url->link('<i class="fa fa-list"></i>', 'listing', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('List')) ?>&nbsp;
                 <?= $this->url->link('<i class="fa fa-calendar"></i>', 'calendar', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Calendar')) ?>&nbsp;
 
                 <?= $this->url->link($this->e($project['name']), 'board', 'show', array('project_id' => $project['id'])) ?>
--- app/Template/project/index.php
+++ app/Template/project/index.php
@@ -11,91 +11,89 @@
             <?php endif ?>
         </ul>
     </div>
-    <section>
-        <?php if ($paginator->isEmpty()): ?>
-            <p class="alert"><?= t('No project') ?></p>
-        <?php else: ?>
-            <table class="table-stripped table-small">
-                <tr>
-                    <th class="column-3"><?= $paginator->order(t('Id'), 'id') ?></th>
-                    <th class="column-5"><?= $paginator->order(t('Status'), 'is_active') ?></th>
-                    <th class="column-15"><?= $paginator->order(t('Project'), 'name') ?></th>
-                    <th class="column-8"><?= $paginator->order(t('Start date'), 'start_date') ?></th>
-                    <th class="column-8"><?= $paginator->order(t('End date'), 'end_date') ?></th>
-                    <?php if ($this->user->isAdmin() || $this->user->isProjectAdmin()): ?>
-                        <th class="column-12"><?= t('Managers') ?></th>
-                        <th class="column-12"><?= t('Members') ?></th>
+    <?php if ($paginator->isEmpty()): ?>
+        <p class="alert"><?= t('No project') ?></p>
+    <?php else: ?>
+        <table class="table-stripped table-small">
+            <tr>
+                <th class="column-3"><?= $paginator->order(t('Id'), 'id') ?></th>
+                <th class="column-5"><?= $paginator->order(t('Status'), 'is_active') ?></th>
+                <th class="column-15"><?= $paginator->order(t('Project'), 'name') ?></th>
+                <th class="column-8"><?= $paginator->order(t('Start date'), 'start_date') ?></th>
+                <th class="column-8"><?= $paginator->order(t('End date'), 'end_date') ?></th>
+                <?php if ($this->user->isAdmin() || $this->user->isProjectAdmin()): ?>
+                    <th class="column-12"><?= t('Managers') ?></th>
+                    <th class="column-12"><?= t('Members') ?></th>
+                <?php endif ?>
+                <th><?= t('Columns') ?></th>
+            </tr>
+            <?php foreach ($paginator->getCollection() as $project): ?>
+            <tr>
+                <td>
+                    <?= $this->url->link('#'.$project['id'], 'board', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link') ?>
+                </td>
+                <td>
+                    <?php if ($project['is_active']): ?>
+                        <?= t('Active') ?>
+                    <?php else: ?>
+                        <?= t('Inactive') ?>
                     <?php endif ?>
-                    <th><?= t('Columns') ?></th>
-                </tr>
-                <?php foreach ($paginator->getCollection() as $project): ?>
-                <tr>
-                    <td>
-                        <?= $this->url->link('#'.$project['id'], 'board', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link') ?>
-                    </td>
-                    <td>
-                        <?php if ($project['is_active']): ?>
-                            <?= t('Active') ?>
-                        <?php else: ?>
-                            <?= t('Inactive') ?>
-                        <?php endif ?>
-                    </td>
-                    <td>
-                        <?= $this->url->link('<i class="fa fa-th"></i>', 'board', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Board')) ?>
-                        <?= $this->url->link('<i class="fa fa-sliders fa-fw"></i>', 'gantt', 'project', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Gantt chart')) ?>
+                </td>
+                <td>
+                    <?= $this->url->link('<i class="fa fa-th"></i>', 'board', 'show', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Board')) ?>
+                    <?= $this->url->link('<i class="fa fa-sliders fa-fw"></i>', 'gantt', 'project', array('project_id' => $project['id']), false, 'dashboard-table-link', t('Gantt chart')) ?>
 
-                        <?php if ($project['is_public']): ?>
-                            <i class="fa fa-share-alt fa-fw" title="<?= t('Shared project') ?>"></i>
-                        <?php endif ?>
-                        <?php if ($project['is_private']): ?>
-                            <i class="fa fa-lock fa-fw" title="<?= t('Private project') ?>"></i>
-                        <?php endif ?>
+                    <?php if ($project['is_public']): ?>
+                        <i class="fa fa-share-alt fa-fw" title="<?= t('Shared project') ?>"></i>
+                    <?php endif ?>
+                    <?php if ($project['is_private']): ?>
+                        <i class="fa fa-lock fa-fw" title="<?= t('Private project') ?>"></i>
+                    <?php endif ?>
 
-                        <?php if (! empty($project['description'])): ?>
-                            <span class="tooltip" title='<?= $this->e($this->text->markdown($project['description'])) ?>'>
-                                <i class="fa fa-info-circle"></i>
-                            </span>
-                        <?php endif ?>
+                    <?php if (! empty($project['description'])): ?>
+                        <span class="tooltip" title='<?= $this->e($this->text->markdown($project['description'])) ?>'>
+                            <i class="fa fa-info-circle"></i>
+                        </span>
+                    <?php endif ?>
 
-                        <?= $this->url->link($this->e($project['name']), 'project', 'show', array('project_id' => $project['id'])) ?>
-                    </td>
-                    <td>
-                        <?= $project['start_date'] ?>
-                    </td>
-                    <td>
-                        <?= $project['end_date'] ?>
-                    </td>
-                    <?php if ($this->user->isAdmin() || $this->user->isProjectAdmin()): ?>
-                    <td>
+                    <?= $this->url->link($this->e($project['name']), 'project', 'show', array('project_id' => $project['id'])) ?>
+                </td>
+                <td>
+                    <?= $project['start_date'] ?>
+                </td>
+                <td>
+                    <?= $project['end_date'] ?>
+                </td>
+                <?php if ($this->user->isAdmin() || $this->user->isProjectAdmin()): ?>
+                <td>
+                    <ul class="no-bullet">
+                    <?php foreach ($project['managers'] as $user_id => $user_name): ?>
+                        <li><?= $this->url->link($this->e($user_name), 'projectuser', 'opens', array('user_id' => $user_id)) ?></li>
+                    <?php endforeach ?>
+                    </ul>
+                </td>
+                <td>
+                    <?php if ($project['is_everybody_allowed'] == 1): ?>
+                        <?= t('Everybody') ?>
+                    <?php else: ?>
                         <ul class="no-bullet">
-                        <?php foreach ($project['managers'] as $user_id => $user_name): ?>
+                        <?php foreach ($project['members'] as $user_id => $user_name): ?>
                             <li><?= $this->url->link($this->e($user_name), 'projectuser', 'opens', array('user_id' => $user_id)) ?></li>
                         <?php endforeach ?>
                         </ul>
-                    </td>
-                    <td>
-                        <?php if ($project['is_everybody_allowed'] == 1): ?>
-                            <?= t('Everybody') ?>
-                        <?php else: ?>
-                            <ul class="no-bullet">
-                            <?php foreach ($project['members'] as $user_id => $user_name): ?>
-                                <li><?= $this->url->link($this->e($user_name), 'projectuser', 'opens', array('user_id' => $user_id)) ?></li>
-                            <?php endforeach ?>
-                            </ul>
-                        <?php endif ?>
-                    </td>
                     <?php endif ?>
-                    <td class="dashboard-project-stats">
-                        <?php foreach ($project['columns'] as $column): ?>
-                            <strong title="<?= t('Task count') ?>"><?= $column['nb_tasks'] ?></strong>
-                            <span><?= $this->e($column['title']) ?></span>
-                        <?php endforeach ?>
-                    </td>
-                </tr>
-                <?php endforeach ?>
-            </table>
+                </td>
+                <?php endif ?>
+                <td class="dashboard-project-stats">
+                    <?php foreach ($project['columns'] as $column): ?>
+                        <strong title="<?= t('Task count') ?>"><?= $column['nb_tasks'] ?></strong>
+                        <span><?= $this->e($column['title']) ?></span>
+                    <?php endforeach ?>
+                </td>
+            </tr>
+            <?php endforeach ?>
+        </table>
 
-            <?= $paginator ?>
-        <?php endif ?>
-    </section>
+        <?= $paginator ?>
+    <?php endif ?>
 </section>
