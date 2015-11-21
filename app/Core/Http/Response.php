37ef9069980a31876d6723da0af1376b510fb668
Frederic Guillot <fred@kanboard.net>
2015-11-21 11:30:45 -0500

    Avoid Safari to append .html at the end of downloaded files

--- app/Core/Http/Response.php
+++ app/Core/Http/Response.php
@@ -47,6 +47,8 @@ class Response extends Base
     public function forceDownload($filename)
     {
         header('Content-Disposition: attachment; filename="'.$filename.'"');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Type: application/octet-stream');
     }
 
     /**
