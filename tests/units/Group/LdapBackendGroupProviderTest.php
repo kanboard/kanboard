a16f3adc1efc2198038325a78bc153daab6db213
Frederic Guillot <fred@kanboard.net>
2015-12-22 19:16:26 +0100

    Add unit test for LdapBackendGroupProvider

---
+++ tests/units/Group/LdapBackendGroupProviderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../Base.php';
+
+use Kanboard\Group\LdapBackendGroupProvider;
+
+class LdapBackendGroupProviderTest extends Base
+{
+    public function testGetLdapGroupPattern()
+    {
+        $this->setExpectedException('LogicException', 'LDAP group filter empty, check the parameter LDAP_GROUP_FILTER');
+
+        $backend = new LdapBackendGroupProvider($this->container);
+        $backend->getLdapGroupPattern('test');
+    }
+}
