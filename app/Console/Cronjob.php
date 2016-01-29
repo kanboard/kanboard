3fd8c5387124cba9a5a2a554f3f707a4c7525b64
Frederic Guillot <fred@kanboard.net>
2016-01-28 20:34:15 -0500

    Remove unused use statement

--- app/Console/Cronjob.php
+++ app/Console/Cronjob.php
@@ -3,7 +3,6 @@
 namespace Kanboard\Console;
 
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\NullOutput;
--- app/Console/TaskTrigger.php
+++ app/Console/TaskTrigger.php
@@ -2,7 +2,6 @@
 
 namespace Kanboard\Console;
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Kanboard\Model\Task;
