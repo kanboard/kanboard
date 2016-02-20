2d27c36a71f08bea60a992b051bfb8a2d8bd06b6
Frederic Guillot <fred@kanboard.net>
2016-02-20 10:46:10 -0500

    Use inline popup to create new columns

--- app/Template/column/index.php
+++ app/Template/column/index.php
@@ -1,5 +1,11 @@
 <div class="page-header">
     <h2><?= t('Edit the board for "%s"', $project['name']) ?></h2>
+    <ul>
+        <li>
+            <i class="fa fa-plus fa-fw"></i>
+            <?= $this->url->link(t('Add a new column'), 'Column', 'create', array('project_id' => $project['id']), false, 'popover') ?>
+        </li>
+    </ul>
 </div>
 
 <?php if (! empty($columns)): ?>
@@ -52,41 +58,3 @@
     </table>
 
 <?php endif ?>
-
-<h3><?= t('Add a new column') ?></h3>
-<form method="post" action="<?= $this->url->href('column', 'create', array('project_id' => $project['id'])) ?>" autocomplete="off">
-
-    <?= $this->form->csrf() ?>
-
-    <?= $this->form->hidden('project_id', $values) ?>
-
-    <?= $this->form->label(t('Title'), 'title') ?>
-    <?= $this->form->text('title', $values, $errors, array('required', 'maxlength="50"')) ?>
-
-    <?= $this->form->label(t('Task limit'), 'task_limit') ?>
-    <?= $this->form->number('task_limit', $values, $errors) ?>
-
-    <?= $this->form->label(t('Description'), 'description') ?>
-
-    <div class="form-tabs">
-        <div class="write-area">
-          <?= $this->form->textarea('description', $values, $errors) ?>
-        </div>
-        <div class="preview-area">
-            <div class="markdown"></div>
-        </div>
-        <ul class="form-tabs-nav">
-            <li class="form-tab form-tab-selected">
-                <i class="fa fa-pencil-square-o fa-fw"></i><a id="markdown-write" href="#"><?= t('Write') ?></a>
-            </li>
-            <li class="form-tab">
-                <a id="markdown-preview" href="#"><i class="fa fa-eye fa-fw"></i><?= t('Preview') ?></a>
-            </li>
-        </ul>
-    </div>
-    <div class="form-help"><?= $this->url->doc(t('Write your text in Markdown'), 'syntax-guide') ?></div>
-
-    <div class="form-actions">
-        <input type="submit" value="<?= t('Add this column') ?>" class="btn btn-blue">
-    </div>
-</form>
