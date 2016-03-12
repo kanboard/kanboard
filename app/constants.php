42c368cb01ae1edb36726a14f3a25539dd3ce7f6
Frederic Guillot <fred@kanboard.net>
2016-03-12 10:09:49 -0500

    Show version number from git-archive

--- app/constants.php
+++ app/constants.php
@@ -8,7 +8,7 @@ defined('DEBUG_FILE') or define('DEBUG_FILE', getenv('DEBUG_FILE') ?: __DIR__.DI
 defined('PLUGINS_DIR') or define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'plugins');
 
 // Application version
-defined('APP_VERSION') or define('APP_VERSION', 'master');
+defined('APP_VERSION') or define('APP_VERSION', build_app_version('$Format:%d$', '$Format:%H$'));
 
 // Database driver: sqlite, mysql or postgres
 defined('DB_DRIVER') or define('DB_DRIVER', 'sqlite');
--- app/functions.php
+++ app/functions.php
@@ -3,6 +3,32 @@
 use Kanboard\Core\Translator;
 
 /**
+ * Build version number from git-archive output
+ *
+ * @param  string $ref
+ * @param  string $commit_hash
+ * @return string
+ */
+function build_app_version($ref, $commit_hash)
+{
+    $version = 'master';
+
+    if ($ref !== '$Format:%d$') {
+        $tag = preg_replace('/\s*\(.*tag:\sv([^,]+).*\)/i', '\1', $ref);
+
+        if (!is_null($tag) && $tag !== $ref) {
+            return $tag;
+        }
+    }
+
+    if ($commit_hash !== '$Format:%H$') {
+        $version .= '.'.$commit_hash;
+    }
+
+    return $version;
+}
+
+/**
  * Translate a string
  *
  * @return string
