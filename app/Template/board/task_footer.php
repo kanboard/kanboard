5672a8c3625d54f66f9bbf10da010a35dc9e95ff
Frederic Guillot <fred@kanboard.net>
2016-05-08 11:56:14 -0400

    Fix broken link

--- app/Template/board/task_footer.php
+++ app/Template/board/task_footer.php
@@ -6,7 +6,7 @@
         <?php else: ?>
             <?= $this->url->link(
                 $this->text->e($task['category_name']),
-                'boardPopover',
+                'TaskPopover',
                 'changeCategory',
                 array('task_id' => $task['id'], 'project_id' => $task['project_id']),
                 false,
