81b09d60da6a3a2ceda45cc2dfe8e54db203cbb7
Lev Lazinskiy <lev@levlaz.org>
2016-06-09 16:42:47 -0700

    Add column dropdown to edit task
    This allows users to move a task to a different column without having
    to drag it.
    
    fix #1879

--- app/Template/task_modification/edit_task.php
+++ app/Template/task_modification/edit_task.php
@@ -9,6 +9,7 @@
     <div class="form-column">
         <?= $this->form->label(t('Title'), 'title') ?>
         <?= $this->form->text('title', $values, $errors, array('autofocus', 'required', 'maxlength="200"', 'tabindex="1"')) ?>
+        <?= $this->task->selectColumn($columns_list, $values, $errors) ?>
         <?= $this->task->selectAssignee($users_list, $values, $errors) ?>
         <?= $this->task->selectCategory($categories_list, $values, $errors) ?>
         <?= $this->task->selectPriority($project, $values) ?>
