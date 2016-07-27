837173cf935a7f5f9c12c69f131a63e0f181d8f3
Frederic Guillot <fred@kanboard.net>
2016-07-27 18:26:56 -0400

    Fix regression in PR #2439

--- app/Template/task/show.php
+++ app/Template/task/show.php
@@ -7,7 +7,7 @@
     'editable' => $this->user->hasProjectAccess('TaskModificationController', 'edit', $project['id']),
 )) ?>
 
-<?php if(!empty($task['description'])): ?>
+<?php if (!empty($task['description'])): ?>
 <?= $this->hook->render('template:task:show:before-description', array('task' => $task, 'project' => $project)) ?>
 <?= $this->render('task/description', array('task' => $task)) ?>
 <?php endif ?>
@@ -22,7 +22,7 @@
 )) ?>
 <?php endif ?>
 
-<?php if(!empty($internal_links)): ?>
+<?php if (!empty($internal_links)): ?>
 <?= $this->hook->render('template:task:show:before-internal-links', array('task' => $task, 'project' => $project)) ?>
 <?= $this->render('task_internal_link/show', array(
     'task' => $task,
@@ -34,7 +34,7 @@
 )) ?>
 <?php endif ?>
 
-<?php if(!empty($external_links)): ?>
+<?php if (!empty($external_links)): ?>
 <?= $this->hook->render('template:task:show:before-external-links', array('task' => $task, 'project' => $project)) ?>
 <?= $this->render('task_external_link/show', array(
     'task' => $task,
@@ -43,7 +43,7 @@
 )) ?>
 <?php endif ?>
 
-<?php if(!empty($files)): ?>
+<?php if (!empty($files) || !empty($images)): ?>
 <?= $this->hook->render('template:task:show:before-attachments', array('task' => $task, 'project' => $project)) ?>
 <?= $this->render('task_file/show', array(
     'task' => $task,
@@ -52,7 +52,7 @@
 )) ?>
 <?php endif ?>
 
-<?php if(!empty($comments)): ?>
+<?php if (!empty($comments)): ?>
 <?= $this->hook->render('template:task:show:before-comments', array('task' => $task, 'project' => $project)) ?>
 <?= $this->render('comments/show', array(
     'task' => $task,
