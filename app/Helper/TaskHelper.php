861441ed31a080a41dd2bfb418c03fc318d3f16e
Rys Sommefeldt <rys@pixeltards.com>
2016-08-01 15:49:43 +0100

    Display priority changes in board view for inverted priority ranges
    Similar to allowing update of priorities when `priority_end` is greater than `priority_start`, to support inverted priority ranges, allow that change to propagate into `formatPriority` and display in the board view.

--- app/Helper/TaskHelper.php
+++ app/Helper/TaskHelper.php
@@ -228,7 +228,7 @@ class TaskHelper extends Base
     {
         $html = '';
 
-        if ($project['priority_end'] > $project['priority_start']) {
+        if ($project['priority_end'] != $project['priority_start']) {
             $html .= '<span class="task-board-priority" title="'.t('Task priority').'">';
             $html .= $task['priority'] >= 0 ? 'P'.$task['priority'] : '-P'.abs($task['priority']);
             $html .= '</span>';
