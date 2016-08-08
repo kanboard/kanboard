fd0bea2187b33e2e90296882dc2e03c2557d65e9
Frederic Guillot <fred@kanboard.net>
2016-08-07 22:52:29 -0400

    Use Sass custom mixins for grid

--- app/Template/header.php
+++ app/Template/header.php
@@ -12,24 +12,15 @@
     )) ?>
 
 <header>
-    <div class="grid grid-reverse grid-noGutter">
-        <?php if (! empty($board_selector)): ?>
-            <div class="col-1 col_xs-4 col_sm-3 col_md-3 col_lg-2 pull-right">
-                <?= $_top_right_corner ?>
-            </div>
-            <div class="col-2 col_xs-8 col_sm-9 col_md-1 col_lg-1">
-                <?= $this->render('header/board_selector', array('board_selector' => $board_selector)) ?>
-            </div>
-            <div class="col-9 col_xs-12 col_sm-12 col_md-8 col_lg-9">
-                <?= $_title ?>
-            </div>
-        <?php else: ?>
-            <div class="col-2 col_xs-12 pull-right">
-                <?= $_top_right_corner ?>
-            </div>
-            <div class="col-10 col_xs-12">
-                <?= $_title ?>
-            </div>
-        <?php endif ?>
+    <div class="title-container">
+        <?= $_title ?>
+    </div>
+    <?php if (! empty($board_selector)): ?>
+    <div class="board-selector-container">
+        <?= $this->render('header/board_selector', array('board_selector' => $board_selector)) ?>
+    </div>
+    <?php endif ?>
+    <div class="menus-container pull-right">
+        <?= $_top_right_corner ?>
     </div>
 </header>
--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,7 @@ var vendor = {
         'bower_components/fullcalendar/dist/fullcalendar.min.css',
         'bower_components/simplemde/dist/simplemde.min.css',
         'bower_components/font-awesome/css/font-awesome.min.css',
-        'bower_components/c3/c3.min.css',
-        'bower_components/gridlex/dist/gridlex.min.css'
+        'bower_components/c3/c3.min.css'
     ],
     js: [
         'bower_components/jquery/dist/jquery.min.js',
