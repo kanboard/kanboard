7e6ab877d34ebcf5d378adc6c352058eed2227a5
Thomas Stinner <me@stinnux.de>
2016-08-30 10:02:31 +0200

    corrected wrong added file

--- assets/js/src/BoardPolling.js
+++ assets/js/src/BoardPolling.js
@@ -3,14 +3,8 @@ Kanboard.BoardPolling = function(app) {
 };
 
 Kanboard.BoardPolling.prototype.execute = function() {
-<<<<<<< 4130de3ca21bb17e4bbb7d564f2c198fc5285529
     if (this.app.hasId("board-container")) {
         var interval = parseInt($("table.board-project").attr("data-check-interval"));
-=======
-    if (this.app.hasId("board")) {
-        var interval = parseInt($("#board").attr("data-check-interval"));
-
->>>>>>> changed to correct file
         if (interval > 0) {
             window.setInterval(this.check.bind(this), interval * 1000);
         }
@@ -21,38 +15,24 @@ Kanboard.BoardPolling.prototype.check = function() {
     if (this.app.isVisible() && !this.app.get("BoardDragAndDrop").savingInProgress) {
         var self = this;
         this.app.showLoadingIcon();
-<<<<<<< 4130de3ca21bb17e4bbb7d564f2c198fc5285529
         // Poll every board
         var pollsinprogress=0;
         $("table.board-project").each(function() {
           var boardid = $(this).attr("data-project-id");
           var url = $(this).attr("data-check-url");
           pollsinprogress++;
-=======
-
-        // Poll every board
-        $("table[id=board]").each(function() {
-          var boardid = $(this).attr("data-project-id");
-          var url = $(this).attr("data-check-url");
->>>>>>> changed to correct file
             $.ajax({
                 cache: false,
                 url: url,
                 statusCode: {
                     200: function(data) {
                         self.app.get("BoardDragAndDrop").refresh(boardid,data);
-<<<<<<< 4130de3ca21bb17e4bbb7d564f2c198fc5285529
                         pollsinprogress--;
                         if (pollsinprogress <= 0) self.app.hideLoadingIcon();
                     },
                     304: function () {
                         pollsinprogress--;
                         if (pollsinprogress <= 0) self.app.hideLoadingIcon();
-=======
-                    },
-                    304: function () {
-                        self.app.hideLoadingIcon();
->>>>>>> changed to correct file
                     }
                 }
             });
