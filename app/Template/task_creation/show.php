a4686877e8d2391ad3a765ad41870ae0dbc05b45
Eskiso <eSkiSo@users.noreply.github.com>
2016-09-28 09:26:24 +0100

    Moved plugin added fields on the first column to position before checkbox
    For templating reasons, when adding new fields to the first column they should be between tag selection and the checkbox.

--- app/Template/task_creation/show.php
+++ app/Template/task_creation/show.php
@@ -10,13 +10,14 @@
             <?= $this->task->selectTitle($values, $errors) ?>
             <?= $this->task->selectDescription($values, $errors) ?>
             <?= $this->task->selectTags($project) ?>
-
+            
+            <?= $this->hook->render('template:task:form:first-column', array('values' => $values, 'errors' => $errors)) ?>
+            
             <?php if (! isset($duplicate)): ?>
                 <?= $this->form->checkbox('another_task', t('Create another task'), 1, isset($values['another_task']) && $values['another_task'] == 1) ?>
                 <?= $this->form->checkbox('duplicate_multiple_projects', t('Duplicate to multiple projects'), 1) ?>
             <?php endif ?>
 
-            <?= $this->hook->render('template:task:form:first-column', array('values' => $values, 'errors' => $errors)) ?>
         </div>
 
         <div class="form-column">
