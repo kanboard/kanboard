71ad04cd66531aa29d8859d7115ec47acf8b89c3
Frederic Guillot <fred@kanboard.net>
2016-10-09 20:35:30 -0400

    Add filter tag:none

--- app/Filter/TaskTagFilter.php
+++ app/Filter/TaskTagFilter.php
@@ -56,12 +56,11 @@ class TaskTagFilter extends BaseFilter implements FilterInterface
      */
     public function apply()
     {
-        $task_ids = $this->db
-            ->table(TagModel::TABLE)
-            ->ilike(TagModel::TABLE.'.name', $this->value)
-            ->asc(TagModel::TABLE.'.project_id')
-            ->join(TaskTagModel::TABLE, 'tag_id', 'id')
-            ->findAllByColumn(TaskTagModel::TABLE.'.task_id');
+        if ($this->value === 'none') {
+            $task_ids = $this->getTaskIdsWithoutTags();
+        } else {
+            $task_ids = $this->getTaskIdsWithGivenTag();
+        }
 
         if (empty($task_ids)) {
             $task_ids = array(-1);
@@ -70,5 +69,25 @@ class TaskTagFilter extends BaseFilter implements FilterInterface
         $this->query->in(TaskModel::TABLE.'.id', $task_ids);
 
         return $this;
+    }
+
+    protected function getTaskIdsWithoutTags()
+    {
+        return $this->db
+            ->table(TaskModel::TABLE)
+            ->asc(TaskModel::TABLE . '.project_id')
+            ->left(TaskTagModel::TABLE, 'tg', 'task_id', TaskModel::TABLE, 'id')
+            ->isNull('tg.tag_id')
+            ->findAllByColumn(TaskModel::TABLE . '.id');
+    }
+
+    protected function getTaskIdsWithGivenTag()
+    {
+        return $this->db
+            ->table(TagModel::TABLE)
+            ->ilike(TagModel::TABLE.'.name', $this->value)
+            ->asc(TagModel::TABLE.'.project_id')
+            ->join(TaskTagModel::TABLE, 'tag_id', 'id')
+            ->findAllByColumn(TaskTagModel::TABLE.'.task_id');
     }
 }
