6135920a7904882ce52301e422743cf2c8f24049
Frederic Guillot <fred@kanboard.net>
2016-12-01 20:02:48 -0500

    Replace notifications Javascript code by CSS

--- assets/js/src/Notification.js
+++
@@ -1,9 +0,0 @@
-Kanboard.Notification = function(app) {
-    this.app = app;
-};
-
-Kanboard.Notification.prototype.execute = function() {
-    $(".alert-fade-out").delay(4000).fadeOut(800, function() {
-        $(this).remove();
-    });
-};
