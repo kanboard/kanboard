5bd19ee64d0cb5a3ebb434e90e0cdeeb60295c49
Frederic Guillot <fred@kanboard.net>
2017-01-08 21:51:43 -0500

    Improve user views layout

--- app/Template/user_credential/password.php
+++ app/Template/user_credential/password.php
@@ -6,14 +6,16 @@
     <?= $this->form->hidden('id', $values) ?>
     <?= $this->form->csrf() ?>
 
-    <?= $this->form->label(t('Current password for the user "%s"', $this->user->getFullname()), 'current_password') ?>
-    <?= $this->form->password('current_password', $values, $errors) ?>
+    <fieldset>
+        <?= $this->form->label(t('Current password for the user "%s"', $this->user->getFullname()), 'current_password') ?>
+        <?= $this->form->password('current_password', $values, $errors) ?>
 
-    <?= $this->form->label(t('New password for the user "%s"', $this->user->getFullname($user)), 'password') ?>
-    <?= $this->form->password('password', $values, $errors) ?>
+        <?= $this->form->label(t('New password for the user "%s"', $this->user->getFullname($user)), 'password') ?>
+        <?= $this->form->password('password', $values, $errors) ?>
 
-    <?= $this->form->label(t('Confirmation'), 'confirmation') ?>
-    <?= $this->form->password('confirmation', $values, $errors) ?>
+        <?= $this->form->label(t('Confirmation'), 'confirmation') ?>
+        <?= $this->form->password('confirmation', $values, $errors) ?>
+    </fieldset>
 
     <div class="form-actions">
         <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
--- app/Template/user_modification/show.php
+++ app/Template/user_modification/show.php
@@ -2,29 +2,36 @@
     <h2><?= t('Edit user') ?></h2>
 </div>
 <form method="post" action="<?= $this->url->href('UserModificationController', 'save', array('user_id' => $user['id'])) ?>" autocomplete="off">
-
     <?= $this->form->csrf() ?>
-
     <?= $this->form->hidden('id', $values) ?>
 
-    <?= $this->form->label(t('Username'), 'username') ?>
-    <?= $this->form->text('username', $values, $errors, array('required', isset($values['is_ldap_user']) && $values['is_ldap_user'] == 1 ? 'readonly' : '', 'maxlength="50"')) ?>
+    <fieldset>
+        <legend><?= t('Profile') ?></legend>
+        <?= $this->form->label(t('Username'), 'username') ?>
+        <?= $this->form->text('username', $values, $errors, array('required', isset($values['is_ldap_user']) && $values['is_ldap_user'] == 1 ? 'readonly' : '', 'maxlength="50"')) ?>
 
-    <?= $this->form->label(t('Name'), 'name') ?>
-    <?= $this->form->text('name', $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_name') ? '' : 'readonly')) ?>
+        <?= $this->form->label(t('Name'), 'name') ?>
+        <?= $this->form->text('name', $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_name') ? '' : 'readonly')) ?>
 
-    <?= $this->form->label(t('Email'), 'email') ?>
-    <?= $this->form->email('email', $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_email') ? '' : 'readonly')) ?>
+        <?= $this->form->label(t('Email'), 'email') ?>
+        <?= $this->form->email('email', $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_email') ? '' : 'readonly')) ?>
+    </fieldset>
 
-    <?= $this->form->label(t('Timezone'), 'timezone') ?>
-    <?= $this->form->select('timezone', $timezones, $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_timezone') ? '' : 'disabled')) ?>
+    <fieldset>
+        <legend><?= t('Preferences') ?></legend>
+        <?= $this->form->label(t('Timezone'), 'timezone') ?>
+        <?= $this->form->select('timezone', $timezones, $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_timezone') ? '' : 'disabled')) ?>
 
-    <?= $this->form->label(t('Language'), 'language') ?>
-    <?= $this->form->select('language', $languages, $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_language') ? '' : 'disabled')) ?>
+        <?= $this->form->label(t('Language'), 'language') ?>
+        <?= $this->form->select('language', $languages, $values, $errors, array($this->user->hasAccess('UserModificationController', 'show/edit_language') ? '' : 'disabled')) ?>
+    </fieldset>
 
     <?php if ($this->user->isAdmin()): ?>
-        <?= $this->form->label(t('Role'), 'role') ?>
+    <fieldset>
+        <legend><?= t('Security') ?></legend>
+        <?= $this->form->label(t('Application role'), 'role') ?>
         <?= $this->form->select('role', $roles, $values, $errors) ?>
+    </fieldset>
     <?php endif ?>
 
     <div class="form-actions">
