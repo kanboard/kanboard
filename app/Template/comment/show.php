17ce51f63e06b31501a53f5e6edd5bd53e2b9fa7
Lev Lazinskiy <lev@levlaz.org>
2017-01-20 18:25:31 -0800

    Make fields translatable

--- app/Template/comment/show.php
+++ app/Template/comment/show.php
@@ -7,8 +7,8 @@
             <strong class="comment-username"><?= $this->text->e($comment['name'] ?: $comment['username']) ?></strong>
         <?php endif ?>
 
-        <small class="comment-date">Created At: <?= $this->dt->datetime($comment['date_creation']) ?></small>
-        <small class="comment-date">Updated At: <?= $this->dt->datetime($comment['date_modification']) ?></small>
+        <small class="comment-date"><?= t('Created At')?>: <?= $this->dt->datetime($comment['date_creation']) ?></small>
+        <small class="comment-date"><?= t('Updated At')?>: <?= $this->dt->datetime($comment['date_modification']) ?></small>
 
     </div>
 
