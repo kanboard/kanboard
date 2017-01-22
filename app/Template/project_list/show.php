2f43d365a0aaeff638eb4731bcff6907b491633a
Frederic Guillot <fred@kanboard.net>
2017-01-22 18:55:22 -0500

    Add project creation links to project management pages

--- app/Template/project_list/show.php
+++ app/Template/project_list/show.php
@@ -2,12 +2,26 @@
     <div class="page-header">
         <ul>
             <?= $this->hook->render('template:project-list:menu:before') ?>
+
+            <?php if ($this->user->hasAccess('ProjectCreationController', 'create')): ?>
+                <li>
+                    <?= $this->modal->medium('plus', t('New project'), 'ProjectCreationController', 'create') ?>
+                </li>
+            <?php endif ?>
+            <?php if ($this->app->config('disable_private_project', 0) == 0): ?>
+                <li>
+                    <?= $this->modal->medium('lock', t('New private project'), 'ProjectCreationController', 'createPrivate') ?>
+                </li>
+            <?php endif ?>
+
             <?php if ($this->user->hasAccess('ProjectUserOverviewController', 'managers')): ?>
                 <li><?= $this->url->icon('user', t('Users overview'), 'ProjectUserOverviewController', 'managers') ?></li>
             <?php endif ?>
+
             <?php if ($this->user->hasAccess('ProjectGanttController', 'show')): ?>
                 <li><?= $this->url->icon('sliders', t('Projects Gantt chart'), 'ProjectGanttController', 'show') ?></li>
             <?php endif ?>
+
             <?= $this->hook->render('template:project-list:menu:after') ?>
         </ul>
     </div>
--- app/Template/project_user_overview/layout.php
+++ app/Template/project_user_overview/layout.php
@@ -1,6 +1,16 @@
 <section id="main">
     <div class="page-header">
         <ul>
+            <?php if ($this->user->hasAccess('ProjectCreationController', 'create')): ?>
+                <li>
+                    <?= $this->modal->medium('plus', t('New project'), 'ProjectCreationController', 'create') ?>
+                </li>
+            <?php endif ?>
+            <?php if ($this->app->config('disable_private_project', 0) == 0): ?>
+                <li>
+                    <?= $this->modal->medium('lock', t('New private project'), 'ProjectCreationController', 'createPrivate') ?>
+                </li>
+            <?php endif ?>
             <li>
                 <?= $this->url->icon('folder', t('Projects list'), 'ProjectListController', 'show') ?>
             </li>
