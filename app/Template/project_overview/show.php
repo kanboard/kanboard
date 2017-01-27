55fa15118ff7b9dfc8d9cf770d2b64c9fbbfdebb
Busfreak <martin@middeke.de>
2017-01-27 19:25:06 +0100

    new hook in project_overview/show

--- app/Template/project_overview/show.php
+++ app/Template/project_overview/show.php
@@ -1,6 +1,7 @@
 <section id="main">
     <?= $this->projectHeader->render($project, 'ProjectOverviewController', 'show') ?>
     <?= $this->render('project_overview/columns', array('project' => $project)) ?>
+    <?= $this->hook->render('template:project-overview:before-description', array('project' => $project)) ?>
     <?= $this->render('project_overview/description', array('project' => $project)) ?>
     <?= $this->render('project_overview/attachments', array('project' => $project, 'images' => $images, 'files' => $files)) ?>
     <?= $this->render('project_overview/information', array('project' => $project, 'users' => $users, 'roles' => $roles)) ?>
