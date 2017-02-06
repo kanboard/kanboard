3e1b1e02494f7bc4512a355cc6a198b7786f8cfe
Frederic Guillot <fred@kanboard.net>
2017-02-05 19:34:12 -0500

    Improve LDAP error reporting

---
+++ app/Core/Ldap/ConnectionException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kanboard\Core\Ldap;
+
+use Exception;
+
+/**
+ * LDAP Connection Exception
+ *
+ * @package ldap
+ * @author  Frederic Guillot
+ */
+class ConnectionException extends Exception
+{
+}
