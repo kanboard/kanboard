2751b200d58e591bf0a838de662443c638fa61f8
Frederic Guillot <fred@kanboard.net>
2017-02-18 19:38:15 -0500

    Add link to send comment by email in comment creation form

--- app/Template/comment/create.php
+++ app/Template/comment/create.php
@@ -1,5 +1,10 @@
 <div class="page-header">
     <h2><?= t('Add a comment') ?></h2>
+    <ul>
+        <li>
+            <?= $this->modal->medium('paper-plane', t('Send by email'), 'CommentMailController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+        </li>
+    </ul>
 </div>
 <form method="post" action="<?= $this->url->href('CommentController', 'save', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
--- app/Template/comment_mail/create.php
+++ app/Template/comment_mail/create.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-    <h2><?= t('Create and send comment by email') ?></h2>
+    <h2><?= t('Create and send a comment by email') ?></h2>
 </div>
 <form method="post" action="<?= $this->url->href('CommentMailController', 'save', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
