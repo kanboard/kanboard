30cdeaa689eae4f6ab10196a3148a2137e7361c5
Thomas Andres <thomas@andres.in>
2017-03-12 18:03:09 +0100

    Changed TaskValidator to accept all date/time formats for date_started
    The TaskValidator did only accept the date/time format defined in the application configuration.

--- app/Validator/TaskValidator.php
+++ app/Validator/TaskValidator.php
@@ -43,7 +43,7 @@ class TaskValidator extends BaseValidator
             new Validators\MaxLength('title', t('The maximum length is %d characters', 200), 200),
             new Validators\MaxLength('reference', t('The maximum length is %d characters', 50), 50),
             new Validators\Date('date_due', t('Invalid date'), $this->dateParser->getParserFormats()),
-            new Validators\Date('date_started', t('Invalid date'), array($this->dateParser->getUserDateTimeFormat())),
+            new Validators\Date('date_started', t('Invalid date'), $this->dateParser->getDateTimeFormats(true)),
             new Validators\Numeric('time_spent', t('This value must be numeric')),
             new Validators\Numeric('time_estimated', t('This value must be numeric')),
         );
