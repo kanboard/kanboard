6e4c40bafc1727125edfda5b508c8f431db7ddcd
Frederic Guillot <fred@kanboard.net>
2017-03-12 21:38:03 -0400

    Redirect to project list when removing a project

--- app/Controller/ProjectStatusController.php
+++ app/Controller/ProjectStatusController.php
@@ -95,6 +95,6 @@ class ProjectStatusController extends BaseController
             $this->flash->failure(t('Unable to remove this project.'));
         }
 
-        $this->response->redirect($this->helper->url->to('ProjectListController', 'show'), true);
+        $this->response->redirect($this->helper->url->to('ProjectListController', 'show'));
     }
 }
