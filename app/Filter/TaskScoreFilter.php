a53ead7e74924579db243bc43125ac52aeb86b91
Yannick Armand <yannick.armand@protonmail.com>
2017-10-14 20:31:41 +0200

    Add filter by score/complexity

---
+++ app/Filter/TaskScoreFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kanboard\Filter;
+
+use Kanboard\Core\Filter\FilterInterface;
+use Kanboard\Model\TaskModel;
+
+/**
+ * Class TaskScoreFilter
+ *
+ * @package Kanboard\Filter
+ */
+class TaskScoreFilter extends BaseComparisonFilter implements FilterInterface
+{
+    /**
+     * Get search attribute
+     *
+     * @access public
+     * @return string[]
+     */
+    public function getAttributes()
+    {
+        return array('score', 'complexity');
+    }
+
+    /**
+     * Apply filter
+     *
+     * @access public
+     * @return FilterInterface
+     */
+    public function apply()
+    {
+        $this->applyComparisonFilter(TaskModel::TABLE.'.score');
+
+        return $this;
+    }
+}
