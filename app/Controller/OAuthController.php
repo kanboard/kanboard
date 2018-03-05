a991758e98fc614d35ec67ec904c78fa0ff6feeb
kent1 <kent1@arscenic.info>
2018-03-05 19:43:15 +0100

    Redirect to original URL after oauth login

--- app/Controller/OAuthController.php
+++ app/Controller/OAuthController.php
@@ -106,7 +106,7 @@ class OAuthController extends BaseController
     protected function authenticate($providerName)
     {
         if ($this->authenticationManager->oauthAuthentication($providerName)) {
-            $this->response->redirect($this->helper->url->to('DashboardController', 'show'));
+            $this->redirectAfterLogin();
         } else {
             $this->authenticationFailure(t('External authentication failed'));
         }
