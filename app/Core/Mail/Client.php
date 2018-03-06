dc9d326bea95c730c1f215b4ea29ea2876a10b00
Rens Sikma <rens@atcomputing.nl>
2018-03-06 09:42:12 +0100

    incorporate feedback

--- app/Core/Mail/Client.php
+++ app/Core/Mail/Client.php
@@ -44,7 +44,7 @@ class Client extends Base
      * @param  string  $html
      * @return Client
      */
-    public function send($recipientEmail, $recipientName, $subject, $html, $authorName=null, $authorEmail=null)
+    public function send($recipientEmail, $recipientName, $subject, $html, $authorName = null, $authorEmail = null)
     {
         if (! empty($recipientEmail)) {
             $this->queueManager->push(EmailJob::getInstance($this->container)->withParams(
@@ -52,8 +52,8 @@ class Client extends Base
                 $recipientName,
                 $subject,
                 $html,
-                (is_null ($authorName) ? $this->getAuthorName() : $authorName),
-                (is_null ($authorEmail)? $this->getAuthorEmail(): $authorEmail)
+                is_null($authorName) ? $this->getAuthorName() : $authorName,
+                is_null($authorEmail) ? $this->getAuthorEmail() : $authorEmail
             ));
         }
 
