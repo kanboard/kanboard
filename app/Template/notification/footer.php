b03357aef341ad55fe28b487131ff4acc851f554
kent1 <kent1@arscenic.info>
2018-04-16 11:57:46 -0700

    Improve notifications
    - Translate subtask status
    - Translate internal link labels
    - Add link to tasks and projects in overdue notifications

--- app/Template/notification/footer.php
+++ app/Template/notification/footer.php
@@ -2,6 +2,8 @@
 Kanboard
 
 <?php if ($this->app->config('application_url') != ''): ?>
-    - <?= $this->url->absoluteLink(t('view the task on Kanboard'), 'TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+    <?php if (isset($task['id'])): ?>
+        - <?= $this->url->absoluteLink(t('view the task on Kanboard'), 'TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+    <?php endif ?>
     - <?= $this->url->absoluteLink(t('view the board on Kanboard'), 'BoardViewController', 'show', array('project_id' => $task['project_id'])) ?>
 <?php endif ?>
