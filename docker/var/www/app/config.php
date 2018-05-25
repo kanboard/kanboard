e30ae7d5d5ceb3eaab5309b319ffaf67d9ecb2a4
Paul Sweeney <psweeney@travelrepublic.co.uk>
2018-05-25 17:23:24 +0100

    fix config overrides in docker

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -1,4 +1,4 @@
 <?php
 
-define('ENABLE_URL_REWRITE', true);
-define('LOG_DRIVER', 'system');
+defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
+defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
