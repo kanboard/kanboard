644ef550fba356216e4901a28ee8b894d1247913
lastlink <theefunk@gmail.com>
2018-06-01 17:09:51 -0400

    media print
    properly setup print css to use the @media print

--- app/Template/layout.php
+++ app/Template/layout.php
@@ -15,6 +15,7 @@
         <?= $this->asset->colorCss() ?>
         <?= $this->asset->css('assets/css/vendor.min.css') ?>
         <?= $this->asset->css('assets/css/app.min.css') ?>
+        <?= $this->asset->css('assets/css/print.min.css', true, 'print') ?>
         <?= $this->asset->customCss() ?>
 
         <?php if (! isset($not_editable)): ?>
