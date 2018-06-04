04764469e721ce69e8c5a7aba7451e3463cb2da3
Miodrag Tokić <miki@denimo.io>
2018-06-04 14:50:17 +0200

    Run cron jobs via URL
    Kanboard supports running cron jobs via CLI. There are hosting services
    that don't offer CLI access, but they do offer calling a URL
    periodically. This feature is often used as a CLI cron job replacement.
    
    This commit adds a CronjobController called by "/cronjob" URL that will
    execute cron jobs as they were executed via CLI. The URL has public
    access, but is protected using the webhook token. The "/cronjob" URL
    should be called via HTTPS.

--- app/ServiceProvider/AuthenticationProvider.php
+++ app/ServiceProvider/AuthenticationProvider.php
@@ -143,6 +143,7 @@ class AuthenticationProvider implements ServiceProviderInterface
         $acl->add('FeedController', '*', Role::APP_PUBLIC);
         $acl->add('AvatarFileController', array('show', 'image'), Role::APP_PUBLIC);
         $acl->add('UserInviteController', array('signup', 'register'), Role::APP_PUBLIC);
+        $acl->add('CronjobController', array('run'), Role::APP_PUBLIC);
 
         $acl->add('ConfigController', '*', Role::APP_ADMIN);
         $acl->add('TagController', '*', Role::APP_ADMIN);
