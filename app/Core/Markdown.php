kanboard/kanboard

commit c44880a5889ebaaec61001dddd6ba4e0b322d623

Exclude task links and user mentions from nesting

app/Core/Markdown.php
@@ -145,4 +145,17 @@ class Markdown extends Parsedown
             array('task_id' => $task_id)
         );
     }
+
+    /**
+     * Exclude from nesting task links and user mentions for links
+     *
+     * @param array $Excerpt
+     * @return array|null
+     */
+    protected function inlineLink($Excerpt)
+    {
+        $Inline = parent::inlineLink($Excerpt);
+        array_push($Inline['element']['nonNestables'], 'TaskLink', 'UserLink');
+        return $Inline;
+    }
 }

tests/units/Helper/TextHelperTest.php
@@ -50,6 +50,13 @@ class TextHelperTest extends Base
                 'Check that: http://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1732454#1732454'
             )
         );
+
+        $this->assertEquals(
+            '<p><a href="http://localhost">item #123 is here</a></p>',
+            $textHelper->markdown(
+                '[item #123 is here](http://localhost)'
+            )
+        );
     }
 
     public function testMarkdownUserLink()
@@ -100,6 +107,13 @@ class TextHelperTest extends Base
         );
 
         $this->assertEquals('<p>Text @admin @notfound</p>', $textHelper->markdown('Text @admin @notfound', true));
+
+        $this->assertEquals(
+            '<p><a href="http://localhost">mention @admin at localhost</a></p>',
+            $textHelper->markdown(
+                '[mention @admin at localhost](http://localhost)'
+            )
+        );
     }
 
     public function testFormatBytes()

commit 1268c0023d7085fb4d960b869bf79ef0ade0d212

Avoid PHP error in Markdown parser
parent::inlineLink() could returns null or an array.

Bug introduced in commit c44880a.

app/Core/Markdown.php
@@ -155,7 +155,9 @@ class Markdown extends Parsedown
     protected function inlineLink($Excerpt)
     {
         $Inline = parent::inlineLink($Excerpt);
-        array_push($Inline['element']['nonNestables'], 'TaskLink', 'UserLink');
+        if (is_array($Inline)) {
+            array_push($Inline['element']['nonNestables'], 'TaskLink', 'UserLink');
+        }
         return $Inline;
     }
 }