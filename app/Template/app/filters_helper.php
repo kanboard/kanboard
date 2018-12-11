1167384f344c562c92f4278d8ae054931e011e03
vincowl <vince512@free.fr>
2018-12-11 17:20:14 +0100

    'My tasks' should show open and closed tasks
    In a project's board, 'My tasks' should show open and closed tasks. This leads the previous statement too. It is due to the fact that I use several columns ("New", "To be processed", "In progress", "Done"). The "Done" column should contain closed tasks.

--- app/Template/app/filters_helper.php
+++ app/Template/app/filters_helper.php
@@ -3,7 +3,7 @@
     <a href="#" class="dropdown-menu dropdown-menu-link-icon" title="<?= t('Default filters') ?>"><i class="fa fa-filter fa-fw"></i><i class="fa fa-caret-down"></i></a>
     <ul>
         <li><a href="#" class="filter-helper filter-reset" data-filter="<?= isset($reset) ? $reset : '' ?>" title="<?= t('Keyboard shortcut: "%s"', 'r') ?>"><?= t('Reset filters') ?></a></li>
-        <li><a href="#" class="filter-helper" data-filter="status:open assignee:me"><?= t('My tasks') ?></a></li>
+        <li><a href="#" class="filter-helper" data-filter="status:all assignee:me"><?= t('My tasks') ?></a></li>
         <li><a href="#" class="filter-helper" data-filter="status:open assignee:me due:tomorrow"><?= t('My tasks due tomorrow') ?></a></li>
         <li><a href="#" class="filter-helper" data-filter="status:open due:today"><?= t('Tasks due today') ?></a></li>
         <li><a href="#" class="filter-helper" data-filter="status:open due:tomorrow"><?= t('Tasks due tomorrow') ?></a></li>
