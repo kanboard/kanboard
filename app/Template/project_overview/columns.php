005993efccd2a3ca44ee372b7e7984bfa4f9288d
vincowl <vince512@free.fr>
2018-12-12 11:32:14 +0100

    Closed tasks should be added to the count of tasks in project's overview
    Closed tasks should be added to the count of tasks in project's overview. Actually, only open tasks are displayed.

--- app/Template/project_overview/columns.php
+++ app/Template/project_overview/columns.php
@@ -1,7 +1,7 @@
 <div class="project-overview-columns">
     <?php foreach ($columns as $column): ?>
         <div class="project-overview-column">
-            <strong title="<?= t('Task count') ?>"><?= $column['nb_open_tasks'] ?></strong>
+            <strong title="<?= t('Task count') ?>"><?= $column['nb_open_tasks']+$column['nb_closed_tasks'] ?></strong>
             <small><?= $this->text->e($column['title']) ?></small>
         </div>
     <?php endforeach ?>
