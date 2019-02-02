ba5878e7869655feda1983967ba80e7c2e811676
Frédéric Guillot <fred@kanboard.net>
2019-02-02 10:50:22 -0800

    Update ChangeLog

--- config.default.php
+++ config.default.php
@@ -24,7 +24,9 @@ define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
 // Plugins directory URL
 define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
 
-// Enable/Disable plugin installer (Disabled by default for security reason)
+// Enable/Disable plugin installer (Disabled by default for security reasons)
+// There is no code review or any approval process to submit a plugin.
+// This is up to the Kanboard instance owner to validate if a plugin is legit.
 define('PLUGIN_INSTALLER', false);
 
 // Available cache drivers are "file" and "memory"
