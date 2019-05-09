2a104e542be2a93ff4fcf5996c90492f191abc34
Rafael de Camargo <rafaelcamargo@trt15.jus.br>
2019-05-09 12:52:02 -0300

    Fix test TaskCommentFilterTest

--- tests/units/Filter/TaskCommentFilterTest.php
+++ tests/units/Filter/TaskCommentFilterTest.php
@@ -21,8 +21,10 @@ class TaskCommentFilterTest extends Base
         $this->assertEquals(1, $projectModel->create(array('name' => 'Test')));
         $this->assertEquals(1, $taskCreation->create(array('title' => 'Test', 'project_id' => 1)));
         $this->assertEquals(1, $commentModel->create(array('task_id' => 1, 'user_id' => 1, 'comment' => 'This is a test')));
+        $this->assertEquals(2, $commentModel->create(array('task_id' => 1, 'user_id' => 1, 'comment' => 'This is another test')));
 
         $filter = new TaskCommentFilter();
+        $filter->setDatabase($this->container['db']);
         $filter->withQuery($query);
         $filter->withValue('test');
         $filter->apply();
@@ -43,6 +45,7 @@ class TaskCommentFilterTest extends Base
         $this->assertEquals(1, $commentModel->create(array('task_id' => 1, 'user_id' => 1, 'comment' => 'This is a test')));
 
         $filter = new TaskCommentFilter();
+        $filter->setDatabase($this->container['db']);
         $filter->withQuery($query);
         $filter->withValue('foobar');
         $filter->apply();
