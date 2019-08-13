7d523c895858bdcfdd2b17515e1e7b75140ea774
Hitesh <hiteshchandwani08@gmail.com>
2019-08-13 23:22:56 +0530

    naming structure fix

--- app/Template/project_header/views.php
+++ app/Template/project_header/views.php
@@ -1,18 +1,18 @@
 <ul class="views">
     
-    <?= $this->hook->render('template:project-header:view-switcher-before-ProjectOverview', array('project' => $project, 'filters' => $filters)) ?>
+    <?= $this->hook->render('template:project-header:view-switcher-before-oroject-overview', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('ProjectOverviewController') ?>>
         <?= $this->url->icon('eye', t('Overview'), 'ProjectOverviewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-overview', t('Keyboard shortcut: "%s"', 'v o')) ?>
     </li>
 
-    <?= $this->hook->render('template:project-header:view-switcher-before-BoardView', array('project' => $project, 'filters' => $filters)) ?>
+    <?= $this->hook->render('template:project-header:view-switcher-before-board-view', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('BoardViewController') ?>>
         <?= $this->url->icon('th', t('Board'), 'BoardViewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-board', t('Keyboard shortcut: "%s"', 'v b')) ?>
     </li>
 
-    <?= $this->hook->render('template:project-header:view-switcher-before-TaskList', array('project' => $project, 'filters' => $filters)) ?>
+    <?= $this->hook->render('template:project-header:view-switcher-before-task-list', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('TaskListController') ?>>
         <?= $this->url->icon('list', t('List'), 'TaskListController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-listing', t('Keyboard shortcut: "%s"', 'v l')) ?>
