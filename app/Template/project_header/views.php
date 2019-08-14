97a30d662f60aa4b4d2f04fc4686cd277092d434
Hitesh Chandwani <dreamcoder8@users.noreply.github.com>
2019-08-14 06:59:00 +0530

    Add new plugin hooks in view switcher

--- app/Template/project_header/views.php
+++ app/Template/project_header/views.php
@@ -1,10 +1,19 @@
 <ul class="views">
+    
+    <?= $this->hook->render('template:project-header:view-switcher-before-project-overview', array('project' => $project, 'filters' => $filters)) ?>
+
     <li <?= $this->app->checkMenuSelection('ProjectOverviewController') ?>>
         <?= $this->url->icon('eye', t('Overview'), 'ProjectOverviewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-overview', t('Keyboard shortcut: "%s"', 'v o')) ?>
     </li>
+
+    <?= $this->hook->render('template:project-header:view-switcher-before-board-view', array('project' => $project, 'filters' => $filters)) ?>
+
     <li <?= $this->app->checkMenuSelection('BoardViewController') ?>>
         <?= $this->url->icon('th', t('Board'), 'BoardViewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-board', t('Keyboard shortcut: "%s"', 'v b')) ?>
     </li>
+
+    <?= $this->hook->render('template:project-header:view-switcher-before-task-list', array('project' => $project, 'filters' => $filters)) ?>
+
     <li <?= $this->app->checkMenuSelection('TaskListController') ?>>
         <?= $this->url->icon('list', t('List'), 'TaskListController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-listing', t('Keyboard shortcut: "%s"', 'v l')) ?>
     </li>
