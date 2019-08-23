39c6f65e85dad6ac7624f34a89fc7dd54c83ac6d
Haocen <Haocen@users.noreply.github.com>
2019-08-23 11:54:17 -0400

    Disable user scaling to avoid page shrinking when drag&drop on mobile device

--- app/Template/layout.php
+++ app/Template/layout.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="robots" content="noindex,nofollow">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
