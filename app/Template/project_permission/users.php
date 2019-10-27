dcfe301ab8cd53211e6d05b01b8a5f4d554bc2f6
Frédéric Guillot <fred@kanboard.net>
2019-10-26 18:07:21 -0700

    Fixes broken PR #4328

--- app/Template/project_permission/users.php
+++ app/Template/project_permission/users.php
@@ -56,5 +56,5 @@
             <button type="submit" class="btn btn-blue"><?= t('Add') ?></button>
         </form>
     </div>
-    <?= $this->hook->render('template:project-permission:after-adduser'), array($project, $values, $errors) ?>
+    <?= $this->hook->render('template:project-permission:after-adduser', ['project' => $project, 'values' => $values, 'errors' => $errors]) ?>
 <?php endif ?>
