811cdaa518adda138215a71a46ab6f9c16469c6f
manne65-hd <manne@hoffmaenner.net>
2020-03-28 08:44:11 +0100

    Show group-memberships in userlist

--- app/Template/user_list/user_details.php
+++ app/Template/user_list/user_details.php
@@ -10,4 +10,8 @@
     <?php if (! empty($user['email'])): ?>
         <span><a href="mailto:<?= $this->text->e($user['email']) ?>"><?= $this->text->e($user['email']) ?></a></span>
     <?php endif ?>
+
+    <?php if (! empty($this->user->getGroupNames($user['id'])) ): ?>
+        <span><i class="fa fa-fw fa-group aria-hidden="true"></i> <?= $this->user->getGroupNames($user['id']) ?></span>
+    <?php endif ?>
 </div>
