13de25eb5ef3ff9bf5eae4b508f0ba311718af08
Igor Mróz <mrozigor@wp.pl>
2020-04-22 18:57:46 +0200

    Removed vendor directory and added to .gitignore.

--- vendor/swiftmailer/swiftmailer/tests/unit/Swift/Mime/SimpleMimeEntityTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Swift_Mime_SimpleMimeEntityTest extends Swift_Mime_AbstractMimeEntityTest
-{
-    protected function _createEntity($headerFactory, $encoder, $cache)
-    {
-        return new Swift_Mime_SimpleMimeEntity($headerFactory, $encoder, $cache, new Swift_Mime_Grammar());
-    }
-}
