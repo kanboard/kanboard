a0a7a1eb316088070969831658a7933d41258950
Franky Van Liedekerke <liedekef@telenet.be>
2020-05-04 11:56:19 +0200

    Add subtask events to ProjectModificationDateSubscriber
    This allows also subtask updates to be reflected on the dashboard (the % info) when a subtask is completed.

--- app/Subscriber/ProjectModificationDateSubscriber.php
+++ app/Subscriber/ProjectModificationDateSubscriber.php
@@ -4,6 +4,7 @@ namespace Kanboard\Subscriber;
 
 use Kanboard\Event\GenericEvent;
 use Kanboard\Model\TaskModel;
+use Kanboard\Model\SubtaskModel;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class ProjectModificationDateSubscriber extends BaseSubscriber implements EventSubscriberInterface
@@ -11,14 +12,16 @@ class ProjectModificationDateSubscriber extends BaseSubscriber implements EventS
     public static function getSubscribedEvents()
     {
         return array(
-            TaskModel::EVENT_CREATE_UPDATE   => 'execute',
-            TaskModel::EVENT_CLOSE           => 'execute',
-            TaskModel::EVENT_OPEN            => 'execute',
-            TaskModel::EVENT_MOVE_SWIMLANE   => 'execute',
-            TaskModel::EVENT_MOVE_COLUMN     => 'execute',
-            TaskModel::EVENT_MOVE_POSITION   => 'execute',
-            TaskModel::EVENT_MOVE_PROJECT    => 'execute',
-            TaskModel::EVENT_ASSIGNEE_CHANGE => 'execute',
+            TaskModel::EVENT_CREATE_UPDATE    => 'execute',
+            TaskModel::EVENT_CLOSE            => 'execute',
+            TaskModel::EVENT_OPEN             => 'execute',
+            TaskModel::EVENT_MOVE_SWIMLANE    => 'execute',
+            TaskModel::EVENT_MOVE_COLUMN      => 'execute',
+            TaskModel::EVENT_MOVE_POSITION    => 'execute',
+            TaskModel::EVENT_MOVE_PROJECT     => 'execute',
+            TaskModel::EVENT_ASSIGNEE_CHANGE  => 'execute',
+            SubtaskModel::EVENT_CREATE_UPDATE => 'execute',
+            SubtaskModel::EVENT_DELETE        => 'execute',
         );
     }
 
