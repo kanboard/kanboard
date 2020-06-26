d65ef2eab2921df18ee515b9f3919c700d2f5708
plague69 <plague69@gmail.com>
2020-06-26 13:54:00 +1000

    Revert "Run integration tests on Github Actions"
    This reverts commit 2e14fb4d442d85786f47cbb1f3a61f7bd44a04e5.

--- tests/integration/BaseProcedureTest.php
+++ tests/integration/BaseProcedureTest.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
-abstract class BaseProcedureTest extends PHPUnit\Framework\TestCase
+abstract class BaseProcedureTest extends PHPUnit_Framework_TestCase
 {
     protected $app = null;
     protected $admin = null;
