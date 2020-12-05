8ce99db2c2019447e9923a3439cb3f4941d93328
Harry Kakoulidis <harry@xarka.com>
2020-12-05 22:09:57 +0200

    Added an option to send a copy of all generated e-mails to a specific bcc address.

--- config.default.php
+++ config.default.php
@@ -44,6 +44,9 @@ define('MAIL_CONFIGURATION', true);
 // E-mail address used for the "From" header (notifications)
 define('MAIL_FROM', 'replace-me@kanboard.local');
 
+// Bcc email for a copy of all notifications. Comment out if not needed.
+// define('MAIL_BCC', 'replace-me@kanboard.local');
+
 // Mail transport available: "smtp", "sendmail", "mail" (PHP mail function), "postmark", "mailgun", "sendgrid"
 define('MAIL_TRANSPORT', 'mail');
 
