9d90b4c7c19331303d5ca7b10c7e07e5d7cbfca5
ThreeCO <42574949+ThreeCO@users.noreply.github.com>
2020-12-30 09:03:23 +0100

    Update AuthSubscriber.php

--- app/Subscriber/AuthSubscriber.php
+++ app/Subscriber/AuthSubscriber.php
@@ -99,7 +99,7 @@ class AuthSubscriber extends BaseSubscriber implements EventSubscriberInterface
         
         if (! empty($username)) {
             // log login failure in web server log to allow fail2ban usage
-            error_log('Kanboard: user '.$username.' authentication failure with IPAddress: ' $ipAddress);
+            error_log('Kanboard: user '.$username.' authentication failure with IP address: '.$ipAddress);
             $this->userLockingModel->incrementFailedLogin($username);
 
             if ($this->userLockingModel->getFailedLogin($username) > BRUTEFORCE_LOCKDOWN) {
@@ -108,7 +108,7 @@ class AuthSubscriber extends BaseSubscriber implements EventSubscriberInterface
         }
         else {
             // log login failure in web server log to allow fail2ban usage
-            error_log('Kanboard: user Unknown authentication failure with IPAddress: ' $ipAddress);
+            error_log('Kanboard: user Unknown authentication failure with IP address: '.$ipAddress);
         }
     }
 }
