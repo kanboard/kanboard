f105afee03cc0664947279ef31a93a6edc8de0cc
Taeluf <contact@taeluf.com>
2021-02-01 16:26:31 -0600

    Fix path to plugin files when PLUGINS_DIR is something other than DOCUMENT_ROOT/plugins

--- app/Helper/AssetHelper.php
+++ app/Helper/AssetHelper.php
@@ -21,6 +21,7 @@ class AssetHelper extends Base
      */
     public function js($filename, $async = false)
     {
+        $filename = $this->getCorrectAssetFilename($filename);
         return '<script '.($async ? 'async' : '').' defer type="text/javascript" src="'.$this->helper->url->dir().$filename.'?'.filemtime($filename).'"></script>';
     }
 
@@ -34,6 +35,7 @@ class AssetHelper extends Base
      */
     public function css($filename, $is_file = true, $media = 'screen')
     {
+        $filename = $this->getCorrectAssetFilename($filename);
         return '<link rel="stylesheet" href="'.$this->helper->url->dir().$filename.($is_file ? '?'.filemtime($filename) : '').'" media="'.$media.'">';
     }
 
@@ -61,5 +63,28 @@ class AssetHelper extends Base
     public function colorCss()
     {
         return '<style>'.$this->colorModel->getCss().'</style>';
+    }
+
+    /**
+     * Corrects the relative path given by plugins that rely upon the assumption that PLUGINS_DIR always points to DOCUMENT_ROOT/plugins
+     *
+     * @param string $filename The filename given by a template:layout:css or template:layout:js hook
+     * @return string
+     */
+    public function getCorrectAssetFilename($filename){
+        $fn = str_replace('\\', '/', $filename);
+        $fnParts = explode('/', $filename);
+        if ($fnParts[0] != 'plugins') return $filename;
+        $root = realpath(dirname(__DIR__, 2));
+        $pDir = realpath(PLUGINS_DIR);
+        if ($root.'/plugins' == $pDir)return $filename;
+
+        array_shift($fnParts);
+        $relPluginsDirPath = implode('/', $fnParts);
+        $absPath = $pDir.'/'.$relPluginsDirPath;
+        $relKanboardDirPath = $this->helper->file->getRelativePath($root, $absPath);
+        $filename = $relKanboardDirPath;
+
+        return $filename;
     }
 }
--- app/Helper/FileHelper.php
+++ app/Helper/FileHelper.php
@@ -119,4 +119,43 @@ class FileHelper extends Base
 
         return null;
     }
+
+    /**
+     * This function was found on StackOverflow at https://stackoverflow.com/a/2638272/802469 
+     *
+     *
+     */
+    function getRelativePath($from, $to)
+    {
+        // some compatibility fixes for Windows paths
+        $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
+        $to   = is_dir($to)   ? rtrim($to, '\/') . '/'   : $to;
+        $from = str_replace('\\', '/', $from);
+        $to   = str_replace('\\', '/', $to);
+
+        $from     = explode('/', $from);
+        $to       = explode('/', $to);
+        $relPath  = $to;
+
+        foreach($from as $depth => $dir) {
+            // find first non-matching dir
+            if($dir === $to[$depth]) {
+                // ignore this directory
+                array_shift($relPath);
+            } else {
+                // get number of remaining dirs to $from
+                $remaining = count($from) - $depth;
+                if($remaining > 1) {
+                    // add traversals up to first matching dir
+                    $padLength = (count($relPath) + $remaining - 1) * -1;
+                    $relPath = array_pad($relPath, $padLength, '..');
+                    break;
+                } else {
+                    $relPath[0] = './' . $relPath[0];
+                }
+            }
+        }
+        return implode('/', $relPath);
+    }
+
 }
