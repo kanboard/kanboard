bc9672086abe4c8c5fe62d231cf39c8620af48fa
manne65-hd <manne@hoffmaenner.net>
2021-04-18 20:56:40 +0200

    Add a method to get the latest comment for a given task

--- app/Model/CommentModel.php
+++ app/Model/CommentModel.php
@@ -77,6 +77,36 @@ class CommentModel extends Base
     }
 
     /**
+     * Get latest comment for a given task
+     *
+     * @access public
+     * @param  integer  $task_id  Task id
+     * @return array
+     */
+    public function getLatest($task_id)
+    {
+        return $this->db
+            ->table(self::TABLE)
+            ->columns(
+                self::TABLE.'.id',
+                self::TABLE.'.date_creation',
+                self::TABLE.'.date_modification',
+                self::TABLE.'.task_id',
+                self::TABLE.'.user_id',
+                self::TABLE.'.comment',
+                UserModel::TABLE.'.username',
+                UserModel::TABLE.'.name',
+                UserModel::TABLE.'.email',
+                UserModel::TABLE.'.avatar_path'
+            )
+            ->join(UserModel::TABLE, 'id', 'user_id')
+            ->orderBy(self::TABLE.'.date_creation', 'DESC')
+            ->orderBy(self::TABLE.'.id', 'DESC')
+            ->eq(self::TABLE.'.task_id', $task_id)
+            ->findOne();
+    }
+
+    /**
      * Get a comment
      *
      * @access public
