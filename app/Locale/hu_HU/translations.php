06f4611654f50ff3eb061772d77bd3c24504f60c
Balázs Úr <balazs@urbalazs.hu>
2021-05-08 08:23:49 +0200

    Update Hungarian translation

--- app/Locale/hu_HU/translations.php
+++ app/Locale/hu_HU/translations.php
@@ -1424,9 +1424,9 @@ return array(
     'Collapse vertically' => 'Összecsukás függőlegesen',
     'Expand vertically' => 'Kinyitás függőlegesen',
     'MXN - Mexican Peso' => 'MXN – mexikói peso',
-    // 'Estimated vs actual time per column' => '',
+    'Estimated vs actual time per column' => 'Becsült és tényleges idő oszloponként',
     'HUF - Hungarian Forint' => 'HUF – magyar forint',
-    'XBT - Bitcoin' => 'XBT – Bitcoin',
-    // 'You must select a file to upload as your avatar!' => '',
-    // 'The file you uploaded is not a valid image! (Only *.gif, *.jpg, *.jpeg and *.png are allowed!)' => '',
+    'XBT - Bitcoin' => 'XBT – bitcoin',
+    'You must select a file to upload as your avatar!' => 'Ki kell választania egy fájlt a profilképként való feltöltéshez!',
+    'The file you uploaded is not a valid image! (Only *.gif, *.jpg, *.jpeg and *.png are allowed!)' => 'A feltöltött fájl nem érvényes kép! Csak *.gif, *.jpg, *.jpeg és *.png engedélyezett.',
 );
