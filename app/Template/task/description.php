a5e83d79d8bced247d900bff7458f4f2146d51af
Mostafa Ahangarha <ahangarha@riseup.net>
2021-06-08 12:45:34 +0430

    add auto direction for markdown component

--- app/Template/task/description.php
+++ app/Template/task/description.php
@@ -1,7 +1,7 @@
 <details class="accordion-section" <?= empty($task['description']) ? '' : 'open' ?>>
     <summary class="accordion-title"><?= t('Description') ?></summary>
     <div class="accordion-content">
-        <article class="markdown">
+        <article class="markdown" dir="auto">
             <?= $this->text->markdown($task['description'], isset($is_public) && $is_public) ?>
         </article>
     </div>
