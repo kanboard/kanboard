fdbb70e9ede05aae70e971f67fbb4657d2d6e64b
Tomas Dittmann <tomas.dittmann@viewstation.de>
2022-02-04 09:49:57 +0100

    get project_id from task

--- app/Controller/SubtaskConverterController.php
+++ app/Controller/SubtaskConverterController.php
@@ -23,11 +23,10 @@ class SubtaskConverterController extends BaseController
 
     public function save()
     {
-        $project = $this->getProject();
         $task = $this->getTask();
         $subtask = $this->getSubtask($task);
 
-        $task_id = $this->subtaskTaskConversionModel->convertToTask($project['id'], $subtask['id']);
+        $task_id = $this->subtaskTaskConversionModel->convertToTask($task['project_id'], $subtask['id']);
 
         if ($task_id !== false) {
             $this->flash->success(t('Subtask converted to task successfully.'));
@@ -35,6 +34,6 @@ class SubtaskConverterController extends BaseController
             $this->flash->failure(t('Unable to convert the subtask.'));
         }
 
-        $this->response->redirect($this->helper->url->to('TaskViewController', 'show', array('project_id' => $project['id'], 'task_id' => $task_id)), true);
+        $this->response->redirect($this->helper->url->to('TaskViewController', 'show', array('task_id' => $task_id)), true);
     }
 }
