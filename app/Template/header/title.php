e7f9029a8d91b0bc15af90a8a301a34271a6e51b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-27 14:01:25 +0300

    improve title appearance

--- app/Template/header/title.php
+++ app/Template/header/title.php
@@ -2,7 +2,7 @@
     <span class="logo">
         <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
     </span>
-    <span class="title">
+    <span class="title" dir="auto">
         <?php if (! empty($project) && ! empty($task)): ?>
             <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
         <?php else: ?>
