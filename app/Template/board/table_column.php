kanboard/kanboard

commit e745f8a23504368d456a879cc67e9bf2279e72ae

Fix board column header alignment

app/Template/board/table_column.php
@@ -14,7 +14,7 @@
         </div>
 
         <!-- column in expanded mode -->
-        <div class="board-column-expanded">
+        <div class="board-column-expanded-header">
             <?php if (! $not_editable && $this->projectRole->canCreateTaskInColumn($column['project_id'], $column['id'])): ?>
                 <?= $this->task->getNewBoardTaskButton($swimlane, $column) ?>
             <?php endif ?>

app/Template/board/table_column_first.php
@@ -7,7 +7,7 @@
         </div>
 
         <!-- column in expanded mode -->
-        <div class="board-column-expanded">
+        <div class="board-column-expanded-header">
             <span class="board-column-title">
             </span>
 

assets/css/src/board.css
@@ -53,6 +53,11 @@
     display: none
 }
 
+.board-column-expanded-header {
+    display: flex;
+    align-items: center
+}
+
 td.board-column-task-collapsed {
     font-weight: bold;
     background-color: #fbfbfb

commit 1c10adc4eaab01ed6e073b65aac4ee574008cced

Fix broken "Hide this Column" feature
Fixes #5167

Regression introduced by #4922

app/Template/board/table_column.php
@@ -14,7 +14,7 @@
         </div>
 
         <!-- column in expanded mode -->
-        <div class="board-column-expanded-header">
+        <div class="board-column-expanded board-column-expanded-header">
             <?php if (! $not_editable && $this->projectRole->canCreateTaskInColumn($column['project_id'], $column['id'])): ?>
                 <?= $this->task->getNewBoardTaskButton($swimlane, $column) ?>
             <?php endif ?>

app/Template/board/table_column_first.php
@@ -7,7 +7,7 @@
         </div>
 
         <!-- column in expanded mode -->
-        <div class="board-column-expanded-header">
+        <div class="board-column-expanded board-column-expanded-header">
             <span class="board-column-title">
             </span>
 