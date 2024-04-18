5f962f9c3245d89fecc3fb7f38859bbd02aab594
Christoph Reiter <reiter.christoph@gmail.com>
2024-04-18 14:34:42 +0200

    Fix various action executions failing if the duration parameter isn't set
    It's easy to create an action that has nothing set for the duration
    by just going "next" in the UI. This results in an empty string being
    stored in the DB for the duration, which with PHP 8 fails as it is
    multiplied by an integer during the action execution, resulting in:
    
    TypeError: Unsupported operand types: string * int
    
    With older PHP this resulted in a warning and "0" as a result.
    By casting to int explicitely we restore that behaviour and we get 0
    if the string can't be converted.
    
    Fixes #5454

--- app/Action/TaskMoveColumnOnDueDate.php
+++ app/Action/TaskMoveColumnOnDueDate.php
@@ -69,7 +69,7 @@ class TaskMoveColumnOnDueDate extends Base
     public function doAction(array $data)
     {
         $results = array();
-        $min = $this->getParam('duration') * 86400;
+        $min = (int)$this->getParam('duration') * 86400;
 
         foreach ($data['tasks'] as $task) {
             $duration = $task['date_due'] - time();
