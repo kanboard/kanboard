50bdaf5f40837f625a88eca38f150ab1c538b23e
yannis <yannishoareau26@gmail.com>
2024-04-19 16:53:00 +0200

    Refactored and made adjustments for comment visibility

--- app/Template/comment_list/create.php
+++ app/Template/comment_list/create.php
@@ -1,8 +1,26 @@
+<?php use Kanboard\Core\Security\Role;?>
 <div class="page-header">
     <h2><?= t('Add a comment') ?></h2>
 </div>
 <form method="post" action="<?= $this->url->href('CommentListController', 'save', array('task_id' => $task['id'])) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
     <?= $this->form->textEditor('comment', array('project_id' => $task['project_id']), array(), array('required' => true, 'aria-label' => t('New comment'))) ?>
+
+    <?php if ($this->user->getRole() !== Role::APP_USER) {
+        $formName = 'visibility';
+        $visibilityOptions['app-user'] = t('Standard users');
+        $visibilityOptions['app-manager'] = t('Application managers or more');
+    }
+    ?>
+
+    <?php if ($this->user->getRole() === Role::APP_ADMIN) {
+        $visibilityOptions['app-admin'] = t('Administrators');
+    }
+    ?>
+
+    <?php if (isset($visibilityOptions)): ?>
+        <?= $this->form->label(t('Visibility:'), $formName) ?>
+        <?= $this->form->select($formName, $visibilityOptions) ?>
+    <?php endif ?>
     <?= $this->modal->submitButtons() ?>
 </form>
