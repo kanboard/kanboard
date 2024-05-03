50b5362c7890751e588cc1179aceec4d4974a172
yannis <yannishoareau26@gmail.com>
2024-05-03 16:41:57 +0200

    FIX: can't create comment with c shortcut in task because of comment visibility

--- app/Template/comment/create.php
+++ app/Template/comment/create.php
@@ -1,3 +1,4 @@
+<?php use Kanboard\Core\Security\Role; ?>
 <div class="page-header">
     <h2><?= t('Add a comment') ?></h2>
     <ul>
@@ -11,5 +12,25 @@
 
     <?= $this->form->textEditor('comment', $values, $errors, array('autofocus' => true, 'required' => true, 'aria-label' => t('New comment'))) ?>
 
+    <?php
+    $formName = 'visibility';
+    $visibilityOptions['app-user'] = t('Standard users');
+    $attribute[] = ('hidden');
+    ?>
+
+    <?php if ($this->user->getRole() !== Role::APP_USER) {
+        echo $this->form->label(t('Visibility:'), $formName);
+        $attribute = [];
+        $visibilityOptions['app-user'] = t('Standard users');
+        $visibilityOptions['app-manager'] = t('Application managers or more');
+    }
+    ?>
+
+    <?php if ($this->user->getRole() === Role::APP_ADMIN) {
+        $visibilityOptions['app-admin'] = t('Administrators');
+    }
+    ?>
+
+    <?= $this->form->select($formName, $visibilityOptions, array(), array(), $attribute) ?>
     <?= $this->modal->submitButtons() ?>
 </form>
