f95e8f5fabcfdc729fecee15ba55ec938d03aa7c
temp5mr <nonono@localhost.localdomain>
2024-05-14 16:44:32 +0300

    Add link to create a comment before the list

--- app/Template/task_comments/show.php
+++ app/Template/task_comments/show.php
@@ -4,6 +4,9 @@
         <?php if (!isset($is_public) || !$is_public): ?>
             <div class="comment-sorting">
                 <small>
+                    <?php if ($editable): ?>
+                        <?= $this->modal->medium('comment', t('Add a comment'), 'CommentController', 'create', array('task_id' => $task['id'])) ?>
+                    <?php endif ?>
                     <?= $this->url->icon('sort', t('Change sorting'), 'CommentController', 'toggleSorting', array('task_id' => $task['id'], 'csrf_token' => $this->app->getToken()->getReusableCSRFToken())) ?>
                     <?php if ($editable): ?>
                         <?= $this->modal->medium('paper-plane', t('Send by email'), 'CommentMailController', 'create', array('task_id' => $task['id'])) ?>
