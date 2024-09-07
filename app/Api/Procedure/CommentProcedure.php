5cd9130af36e0f0eee19e8b8e96b648fbee94637
Yannis Hoareau <93135242+YannisHoareau@users.noreply.github.com>
2024-09-07 20:53:51 +0200

    fix(api): check comment visibility in API procedures

--- app/Api/Procedure/CommentProcedure.php
+++ app/Api/Procedure/CommentProcedure.php
@@ -22,8 +22,25 @@ class CommentProcedure extends BaseProcedure
 
     public function getAllComments($task_id)
     {
+        $res = array();
         TaskAuthorization::getInstance($this->container)->check($this->getClassName(), 'getAllComments', $task_id);
-        return $this->commentModel->getAll($task_id);
+        $comments = $this->commentModel->getAll($task_id);
+        $user_role = $this->userSession->getRole();
+
+        foreach ($comments as $comment) {
+
+            if ($user_role === Role::APP_MANAGER && $comment['visibility'] === Role::APP_ADMIN) {
+                continue;
+            }
+
+            if ($user_role === Role::APP_USER && $comment['visibility'] !== Role::APP_USER) {
+                continue;
+            }
+
+            $res[]=$comment;
+        }
+
+        return $res;
     }
 
     public function removeComment($comment_id)
@@ -34,6 +51,15 @@ class CommentProcedure extends BaseProcedure
 
     public function createComment($task_id, $user_id, $content, $reference = '', $visibility = Role::APP_USER)
     {
+
+        if ($this->userSession->getRole() === Role::APP_MANAGER && $visibility === Role::APP_ADMIN) {
+            return false;
+        }
+
+        if ($this->userSession->getRole() === Role::APP_USER && $visibility !== Role::APP_USER) {
+            return false;
+        }
+
         TaskAuthorization::getInstance($this->container)->check($this->getClassName(), 'createComment', $task_id);
         
         $values = array(
