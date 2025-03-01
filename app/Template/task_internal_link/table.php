88acc30a2a956866637b4623a6fdf644674cb414
Al-27 <125827852+Al-27@users.noreply.github.com>
2025-03-01 04:33:46 +0000

    fix: internally linked task titles no longer wrap under the dropdown menu icon

--- app/Template/task_internal_link/table.php
+++ app/Template/task_internal_link/table.php
@@ -17,41 +17,43 @@
             <?php endif ?>
         <tr>
             <td>
-                <?php if ($is_public): ?>
-                    <?= $this->url->link(
-                        $this->text->e('#'.$link['task_id'].' '.$link['title']),
-                        'TaskViewController',
-                        'readonly',
-                        array('task_id' => $link['task_id'], 'token' => $project['token']),
-                        false,
-                        $link['is_active'] ? '' : 'task-link-closed'
-                    ) ?>
-                <?php else: ?>
-                    <?php if ($editable && $this->user->hasProjectAccess('Tasklink', 'edit', $task['project_id'])): ?>
-                        <div class="dropdown">
-                            <a href="#" class="dropdown-menu dropdown-menu-link-icon"><i class="fa fa-cog"></i><i class="fa fa-caret-down"></i></a>
-                            <ul>
-                                <li>
-                                    <?= $this->modal->medium('edit', t('Edit'), 'TaskInternalLinkController', 'edit', array('link_id' => $link['id'], 'task_id' => $task['id'])) ?>
-                                </li>
-                                <li>
-                                    <?= $this->modal->confirm('trash-o', t('Remove'), 'TaskInternalLinkController', 'confirm', array('link_id' => $link['id'], 'task_id' => $task['id'])) ?>
-                                </li>
-                            </ul>
-                        </div>
-                    <?php endif ?>
+                <div class="task-links-table-td">
+                        <?php $link_mode = 'show' ?>
+                        <?php $link_array = array() ?>
 
-                    <?= $this->url->link(
-                        $this->text->e('#'.$link['task_id'].' '.$link['title']),
-                        'TaskViewController',
-                        'show',
-                        array('task_id' => $link['task_id']),
-                        false,
-                        $link['is_active'] ? '' : 'task-link-closed'
-                    ) ?>
-                <?php endif ?>
+                        <?php if ($is_public): ?>
+                            <?php $link_mode = 'readonly' ?>
+                            <?php $link_array = array('task_id' => $link['task_id'], 'token' => $project['token']) ?>
+                        <?php else: ?>
+                            <?php $link_array = array('task_id' => $link['task_id']) ?>
+                            <?php if ($editable && $this->user->hasProjectAccess('Tasklink', 'edit', $task['project_id'])): ?>
+                                <div class="dropdown">
+                                    <a href="#" class="dropdown-menu dropdown-menu-link-icon"><i class="fa fa-cog"></i><i class="fa fa-caret-down"></i></a>
+                                    <ul>
+                                        <li>
+                                            <?= $this->modal->medium('edit', t('Edit'), 'TaskInternalLinkController', 'edit', array('link_id' => $link['id'], 'task_id' => $task['id'])) ?>
+                                        </li>
+                                        <li>
+                                            <?= $this->modal->confirm('trash-o', t('Remove'), 'TaskInternalLinkController', 'confirm', array('link_id' => $link['id'], 'task_id' => $task['id'])) ?>
+                                        </li>
+                                    </ul>
+                                </div>
+                            <?php endif ?>                         
+                        <?php endif ?>
 
-                (<?php if ($link['project_id'] != $project['id']): ?><?= $this->text->e($link['project_name']) ?> - <?php endif ?><?= $this->text->e($link['column_title']) ?>)
+                    <div>
+                        <?= $this->url->link(
+                            $this->text->e('#'.$link['task_id'].' '.$link['title']),
+                            'TaskViewController',
+                            $link_mode,
+                            $link_array,
+                            false,
+                            $link['is_active'] ? '' : 'task-link-closed'
+                        ) ?>
+
+                        (<?php if ($link['project_id'] != $project['id']): ?><?= $this->text->e($link['project_name']) ?> - <?php endif ?><?= $this->text->e($link['column_title']) ?>)
+                    </div>
+                </div>
             </td>
             <td>
                 <?php if (! empty($link['task_assignee_username'])): ?>
