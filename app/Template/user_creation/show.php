091848edccdb6b2a98f09c48ed6ad787030620ec
Syratus <34027069+Syratus@users.noreply.github.com>
2025-04-23 13:36:41 +0200

    Update show.php

--- app/Template/user_creation/show.php
+++ app/Template/user_creation/show.php
@@ -54,7 +54,9 @@
                 <?= $this->form->label(t('Filter'), 'filter') ?>
                 <?= $this->form->text('filter', $values, $errors) ?>
 
-                <?= $this->form->checkbox('notifications_enabled', t('Enable email notifications'), 1, isset($values['notifications_enabled']) && $values['notifications_enabled'] == 1 ? true : false) ?>
+                <?php if (ENABLE_NOTIFICATIONS == false): ?>
+                    <?= $this->form->checkbox('notifications_enabled', t('Enable email notifications'), 1, isset($values['notifications_enabled']) && $values['notifications_enabled'] == 1 ? true : false) ?>
+                <?php endif ?>
             </fieldset>
 
             <fieldset>
