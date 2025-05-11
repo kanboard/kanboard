kanboard/kanboard

commit 4b9cad82c932975a5515573398bb65b879178f46

feat: allow attaching screenshots and files when creating a task

app/Controller/TaskCreationController.php
@@ -20,7 +20,7 @@ class TaskCreationController extends BaseController
      * @param  array $errors
      * @throws PageNotFoundException
      */
-    public function show(array $values = array(), array $errors = array())
+    public function show(array $values = array(), $screenshot = '', array $files = array(), array $errors = array())
     {
         $project = $this->getProject();
         $swimlanesList = $this->swimlaneModel->getList($project['id'], false, true);
@@ -37,6 +37,8 @@ class TaskCreationController extends BaseController
             'users_list' => $this->projectUserRoleModel->getAssignableUsersList($project['id'], true, false, $project['is_private'] == 1),
             'categories_list' => $this->categoryModel->getList($project['id']),
             'swimlanes_list' => $swimlanesList,
+            'screenshot' => $screenshot,
+            'files' => $files,
         )));
     }
 
@@ -50,12 +52,15 @@ class TaskCreationController extends BaseController
         $project = $this->getProject();
         $values = $this->request->getValues();
         $values['project_id'] = $project['id'];
+        $files = $this->request->getFileInfo('files');
+        $screenshot = $values['screenshot'];
+        array_splice($values, 13, 1);
 
         list($valid, $errors) = $this->taskValidator->validateCreation($values);
 
         if (! $valid) {
             $this->flash->failure(t('Unable to create your task.'));
-            $this->show($values, $errors);
+            $this->show($values, $screenshot, $files,$errors);
         } else if (! $this->helper->projectRole->canCreateTaskInColumn($project['id'], $values['column_id'])) {
             $this->flash->failure(t('You cannot create tasks in this column.'));
             $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
@@ -63,8 +68,19 @@ class TaskCreationController extends BaseController
             $task_id = $this->taskCreationModel->create($values);
 
             if ($task_id > 0) {
-                $this->flash->success(t('Task created successfully.'));
-                $this->afterSave($project, $values, $task_id);
+                if ($screenshot) {
+                    $this->taskFileModel->uploadScreenshot($task_id, $screenshot);
+                }
+                if ($files['name'][0]) {
+                    $filesUploaded = $this->taskFileModel->uploadFiles($task_id, $files);
+                }
+
+                if ($files['name'][0] && !$filesUploaded) {
+                    $this->flash->failure(t('Unable to upload files, check the permissions of your data folder.'));
+                } else {
+                    $this->flash->success(t('Task created successfully.'));
+                    $this->afterSave($project, $values, $task_id);
+                }
             } else {
                 $this->flash->failure(t('Unable to create this task.'));
                 $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);

app/Helper/TaskHelper.php
@@ -288,6 +288,31 @@ class TaskHelper extends Base
         return $html;
     }
 
+    public function renderFileUpload($screenshot = '', array $files = array())
+    {
+		$upload_max_size = get_upload_max_size()*0.90; // 10% margin for the orther part of request + conversion in base64
+        $html =  '<div class="task-form-bottom-column">';
+        $html .=  '    <div id="screenshot-zone">';
+        $html .=  '        <p id="screenshot-inner">'.t('Take a screenshot and press CTRL+V or ⌘+V to paste here.').'</p>';
+        $html .=  '    </div>';
+        $html .=  '</div>';
+        $html .=  '<div class="task-form-bottom-column">';
+        $html .=  $this->helper->app->component('file-upload-task-create', array(
+            'maxSize'           => $upload_max_size,
+            'labelDropzone'     => t('Drag and drop your files here'),
+            'labelOr'           => t('or'),
+            'labelChooseFiles'  => t('choose files'),
+            'labelOversize'     => t('The total maximum allowed attachments size is %sB. (You will be able to upload more later)', $this->helper->text->bytes($upload_max_size)),
+            'labelSuccess'      => t('All files have been uploaded successfully.'),
+            'labelCloseSuccess' => t('Close this window'),
+            'labelUploadError'  => t('Unable to upload this file.'),
+            'screenshot' => $screenshot,
+            'files' => $files,
+        ));
+        $html .=  '</div>';
+        return $html;
+    }
+
     public function getProgress($task)
     {
         if (! isset($this->columns[$task['project_id']])) {

app/Locale/fr_FR/translations.php
@@ -1465,4 +1465,6 @@ return array(
     'Remove internal link(s)' => 'Enlever le(s) lien(s) interne(s)',
     'Import tasks from another project' => 'Importer les tâches d\'un autre projet',
     'Select the project to copy tasks from' => 'Sélectionner le projet dont vous souhaitez copier les tâches',
+    'The total maximum allowed attachments size is %sB. (You will be able to upload more later)' => 'Taille maximale totale des pièces jointes atteinte %so. (Vous pourrez en ajouter ultérieurement)',
+    'Add attachments' => 'Ajouter des pièces jointes',
 );

app/Template/task_creation/show.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <h2><?= $this->text->e($project['name']) ?> &gt; <?= t('New task') ?></h2>
 </div>
-<form method="post" action="<?= $this->url->href('TaskCreationController', 'save', array('project_id' => $project['id'])) ?>" autocomplete="off">
+<form class="js-modal-ignore-form" method="post" action="<?= $this->url->href('TaskCreationController', 'save', array('project_id' => $project['id'])) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
 
     <div class="task-form-container">
@@ -37,6 +37,13 @@
         </div>
 
         <div class="task-form-bottom">
+
+            <details class="accordion-section">
+                <summary class="accordion-title"><?= t('Add attachments') ?></summary>
+                <div class="accordion-content">
+                    <?= $this->task->renderFileUpload($screenshot, $files) ?>
+                </div>
+            </details>
 
             <?= $this->hook->render('template:task:form:bottom-before-buttons', array('values' => $values, 'errors' => $errors)) ?>
 

assets/css/src/task_form.css
@@ -67,3 +67,10 @@
 .task-form-bottom label {
     display: inline-block
 }
+
+.task-form-bottom-column {
+    display: inline-block;
+    width: 49%;
+    margin-left: 5px;
+    margin-right: 5px;
+}

commit c22223792bd4a1d9f50f282b91d7cf9e2fafd776

fix: avoid SQL error when creating tasks with category
Regression introduced by commit 4b9cad8

app/Controller/TaskCreationController.php
@@ -53,38 +53,44 @@ class TaskCreationController extends BaseController
         $values = $this->request->getValues();
         $values['project_id'] = $project['id'];
         $files = $this->request->getFileInfo('files');
-        $screenshot = $values['screenshot'];
-        array_splice($values, 13, 1);
+
+        $screenshot = null;
+        if (array_key_exists('screenshot', $values)) {
+            $screenshot = $values['screenshot'];
+            unset($values['screenshot']);
+        }
 
         list($valid, $errors) = $this->taskValidator->validateCreation($values);
 
         if (! $valid) {
             $this->flash->failure(t('Unable to create your task.'));
-            $this->show($values, $screenshot, $files,$errors);
+            $this->show($values, $screenshot, $files, $errors);
         } else if (! $this->helper->projectRole->canCreateTaskInColumn($project['id'], $values['column_id'])) {
             $this->flash->failure(t('You cannot create tasks in this column.'));
             $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
         } else {
             $task_id = $this->taskCreationModel->create($values);
-
-            if ($task_id > 0) {
-                if ($screenshot) {
-                    $this->taskFileModel->uploadScreenshot($task_id, $screenshot);
-                }
-                if ($files['name'][0]) {
-                    $filesUploaded = $this->taskFileModel->uploadFiles($task_id, $files);
-                }
-
-                if ($files['name'][0] && !$filesUploaded) {
-                    $this->flash->failure(t('Unable to upload files, check the permissions of your data folder.'));
-                } else {
-                    $this->flash->success(t('Task created successfully.'));
-                    $this->afterSave($project, $values, $task_id);
-                }
-            } else {
+            if ($task_id === 0) {
                 $this->flash->failure(t('Unable to create this task.'));
                 $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
+                return;
             }
+
+            if ($screenshot) {
+                $this->taskFileModel->uploadScreenshot($task_id, $screenshot);
+            }
+
+            if (isset($files['name'][0]) && $files['name'][0] !== '') {
+                $filesUploaded = $this->taskFileModel->uploadFiles($task_id, $files);
+                if (! $filesUploaded) {
+                    $this->flash->failure(t('Unable to upload files, check the permissions of your data folder.'));
+                    $this->response->redirect($this->helper->url->to('BoardViewController', 'show', ['project_id' => $project['id']]), true);
+                    return;
+                }
+            }
+
+            $this->flash->success(t('Task created successfully.'));
+            $this->afterSave($project, $values, $task_id);
         }
     }
 