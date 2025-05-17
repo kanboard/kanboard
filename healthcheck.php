95f280cdf95bbf6d7fa642dbe80bf1dea9343795
Frédéric Guillot <fred@kanboard.net>
2025-05-16 18:18:41 -0700

    feat: add healthcheck endpoint and docker-compose files for MariaDB, Postgres, and SQLite

---
+++ healthcheck.php
@@ -0,0 +1,21 @@
+<?php
+
+function send_response($status, $message) {
+    http_response_code($status);
+    header('Content-Type: application/json');
+    echo json_encode(['status' => $status, 'message' => $message]);
+    exit;
+}
+
+set_error_handler(function($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+set_exception_handler(function($exception) {
+    send_response(503, $exception->getMessage());
+});
+
+require __DIR__.'/app/common.php';
+
+$container['db']->getConnection()->query('SELECT 1');
+send_response(200, 'Database connection is OK');
--- libs/picodb/lib/PicoDb/Driver/Base.php
+++ libs/picodb/lib/PicoDb/Driver/Base.php
@@ -261,5 +261,4 @@ abstract class Base
     {
         return $this->getConnection()->query('SELECT VERSION()')->fetchColumn();
     }
-
 }
