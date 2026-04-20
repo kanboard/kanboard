a0d0282cd42ceb887bc68e604436a71c70887738
Frédéric Guillot <323546+fguillot@users.noreply.github.com>
2026-04-19 18:51:34 -0700

    chore: bump phpunit to 12.5.23 and remove stray parsedown composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'kanboard/kanboard',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9bfb6d14d374d78ecbedf814db3fb54876a4cedb',
+        'reference' => '1a9a32da1118a095325ecec50d4c0a784dcd46f8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'kanboard/kanboard' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9bfb6d14d374d78ecbedf814db3fb54876a4cedb',
+            'reference' => '1a9a32da1118a095325ecec50d4c0a784dcd46f8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
